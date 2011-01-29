4a249214a97094b4578d3d27c21539d1babfb225
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-01-29 18:34:38 +0100

    Opprett en arrangementklasse.
    Denne klassen inneholder informasjon tilsvarende det som ble lagret på
    forrige Nablaweb. Tekstrepresentasjon arves fra Nyhetsklassen,
    foreløpig bare en dummy-klasse.

--- nablaweb/arrangement/models.py
+++ nablaweb/arrangement/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Dummy-klasse
+class Nyhet(models.Model):
+    """class Nyhet(models.Model)"""
+
+    tittel = models.CharField(max_length=100)
+    ingress = models.TextField()
+    brodtekst = models.TextField()
+
+
+class Arrangement(Nyhet):
+    """class Arrangement(Nyhet)"""
+
+    sted = models.CharField(max_length=100)
+    tid = models.DateTimeField()
+
+    aapen = models.BooleanField(default=False)
+    plasser = models.PositiveIntegerField(default=0)
+    paameldte = models.ManyToManyField(User)
+
+    paameldingsfrist = models.BooleanField(default=False)
+    frist = models.DateTimeField()
+
+    ikke_fysmat = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s, %s" % (self.tittel[:20], self.tid.strftime("%d/%m/%y"))
