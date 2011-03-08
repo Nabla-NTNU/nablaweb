5c76922ed5e22102ffbd86dfd70aecd0bab4d51f
Knut Andreas <knutanha@gauss.nt.ntnu.no>
2011-03-08 15:50:52 +0100

    La til det som fungerer i gallery

--- nablaweb/galleri/models.py
+++ nablaweb/galleri/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Mye som mangler her fortsatt
+
+class Album(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=true)
+    def __unicode__(self):
+        return self.title
+
+class Picture(models.Model):
+    title = models.CharField(max_length=64, blank=true) #Går ut i fra at det kan være litt slitsomt å navngi alle bilder
+    description = models.TextField(blank=true)
+    album = models.ForeignKey(Album)
+    picture = models.ImageField(upload_to='images') #Ikke ferdig, her må det selvsagt brukes et mer omfattende opplastingsskript
+    def __unicode__(self):
+        return self.title
--- nablaweb/galleri/views.py
+++ nablaweb/galleri/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.template import Context, loader, RequestContext
+from gallery.models import *
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    album_list = Album.objects.all()
+    return render_to_response('gallery/index.html', {'album_list': album_list})
+
+def album(request, album_id):
+    a = get_object_or_404(Album, pk=album_id)
+    return render_to_response('gallery/album.html', {'album': a})
+
+def picture_large(request, album_id, picture_number):
+
+    album = get_object_or_404(Album, pk=album_id)
+    album_pictures = album.picture_set.all()
+    album_size = len(album_pictures)
+
+    # "Oversetter" til den faktiske id-verdien til bildet
+    picture_number = int(picture_number)
+    picture_id = 0
+    if (picture_number > 0 and picture_number <= album_size):
+        picture_id = album_pictures[picture_number-1].id
+    picture = get_object_or_404(Picture, pk=picture_id)
+
+    return render_to_response('gallery/picture_large.html',
+                             {'picture': picture, 'total': album_size,
+                              'current': picture_number, 'album': album, 
+                              'pic_list': album_pictures})
