d875355270bb939894e28152df417cc2628315d7
Knut Andreas <knutanha@stud.ntnu.no>
2011-03-16 04:52:26 +0100

    Flyttet litt rundt på ting, overlagret save()sjonen til album da stedet for å ha en egen funksjon til å ta seg av dynamisk lagring. Ordnet sånn at opplasting via admin fungerer

--- nablaweb/galleri/models.py
+++ nablaweb/galleri/models.py
@@ -7,7 +7,12 @@ import os
 import hashlib
 import StringIO
 
-GALLERY_DIR = os.path.join(getattr(settings, 'MEDIA_ROOT', None),'gallery')
+GALLERY_FOLDER = 'gallery'
+TEMP_FOLDER = 'temp'
+
+# MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT', None)
+GALLERY_DIR = os.path.join(getattr(settings, 'MEDIA_ROOT', None),GALLERY_FOLDER)
+TEMP_DIR = os.path.join(getattr(settings, 'MEDIA_ROOT', None),TEMP_FOLDER)
 
 """ Innstillinger for opplastede bilders opplosning """
 
@@ -35,52 +40,58 @@ class Picture(models.Model):
     title = models.CharField(max_length=64, blank=True)
     description = models.TextField(blank=True)
     album = models.ForeignKey(Album)
-    picture = models.ImageField(upload_to='temp', blank=True)
-    thumbnail = models.ImageField(upload_to='temp', blank=True)
+    picture = models.ImageField(upload_to=TEMP_FOLDER, blank=True)
+    thumbnail = models.ImageField(upload_to=TEMP_FOLDER, blank=True)
     def __unicode__(self):
         return self.title
 
-# HAX
-    def manage_uploaded_image(self,posted_picture):
-        imagefile = StringIO.StringIO(posted_picture.read())
-        img = Image.open(imagefile)
-        img.load()
-        # For aa oppdage korrupte png-filer (funker ikke atm)
-        #img = Image.open(imagefile)
-        #img.verify()
 
-        # Endrer storrelse
-        (width, height) = img.size
-        resizedImage = img.resize(scale_dimensions(width, height, lowest_side = PICTURE_DIM))
-        thumbnail = img.resize(scale_dimensions(width, height, lowest_side = PICTURE_DIM_THUMB))
+    def save(self):
+        super(Picture, self).save()
+        temp_path = self.picture.path
 
-        imagefile = StringIO.StringIO()
-        resizedImage.save(imagefile, 'JPEG')
-        # Setter filnavn som forhaapentligvis er unikt
-        # Litt tungvint aa maatte lagre to ganger, saa det
-        # kan vaere noe aa fikse paa til senere
-        filename = hashlib.md5(imagefile.getvalue()).hexdigest()
-        filename_thumb = filename + '-thumb.jpg'
-        filename = filename + '.jpg'
-    
-        save_path = os.path.join(GALLERY_DIR,str(self.album.title))
-    
-        try:
-            os.stat(save_path)
-        except:
-            os.makedirs(save_path)
-    
-        imagefile = open(os.path.join(save_path, filename), 'w')
-        resizedImage.save(imagefile, 'JPEG')
-        imagefile.close()
-    
-        imagefile = open(os.path.join(save_path, filename_thumb), 'w')
-        thumbnail.save(imagefile, 'JPEG')
-        imagefile.close()
+        # De to tilfellene der noe maa gjores:
+        if ( (temp_path.find(GALLERY_DIR) == -1) or not self.thumbnail):
+            img = Image.open(temp_path)
             
-        # Django bruker automatisk MEDIA_ROOT som root for ImageFields saa man kan ikke bruke 'save_path'
-        media_path = 'gallery/' + str(self.album.title) + '/'
-        self.picture = media_path + filename
-        self.thumbnail = media_path + filename_thumb
-        self.save()
+            # Endrer storrelse paa bilder
+            (width, height) = img.size
+            resizedImage = img.resize(scale_dimensions(width, height, lowest_side = PICTURE_DIM))
+            thumbnail = img.resize(scale_dimensions(width, height, lowest_side = PICTURE_DIM_THUMB))       
+            
+            # Ordner filnavn og lagringssted
+            imagefile = StringIO.StringIO()
+            resizedImage.save(imagefile, 'JPEG')
+            filename = hashlib.md5(imagefile.getvalue()).hexdigest()
+            filename_thumb = filename + '-thumb.jpg'
+            filename = filename + '.jpg'
+            save_path = os.path.join(GALLERY_DIR,str(self.album.title))
+            
+            # Oppretter mapper om de ikke finnes
+            try:
+                os.stat(save_path)
+            except:
+                os.makedirs(save_path)
+            
+            # Lagrer thumbnail
+            imagefile = open(os.path.join(save_path, filename), 'w')
+            resizedImage.save(imagefile, 'JPEG')
+            imagefile.close()
+            
+            # Lagrer thumbnail
+            imagefile = open(os.path.join(save_path, filename_thumb), 'w')
+            thumbnail.save(imagefile, 'JPEG')
+            imagefile.close()
+                
+            # Django bruker automatisk 'MEDIA_ROOT' som root for ImageFields 
+            # saa man kan ikke bruke 'save_path'
+            media_path = 'gallery/' + str(self.album.title) + '/'
+            self.picture = media_path + filename
+            self.thumbnail = media_path + filename_thumb
+            
+            # Sletter det gamle bildet
+            temp_path = img.filename
+            os.remove(temp_path)
     
+            super(Picture, self).save()
+
