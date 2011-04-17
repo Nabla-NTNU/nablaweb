927acbfa4434b2a4b884672de86f11739ade8c61
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-04-17 20:57:16 +0200

    Fiks urls.py slik at oppretting av nyheter ikke krever et nummer.

--- nablaweb/nyheter/urls.py
+++ nablaweb/nyheter/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('nyheter.views',
     (r'^$', 'list_news'),
     (r'^vis/(?P<news_id>\d+)/$', 'show_news'),
-    (r'^opprett/(?P<news_id>\d+)/$', 'create_news'),
+    (r'^opprett/$', 'create_news'),
     (r'^endre/(?P<news_id>\d+)/$', 'edit_news'),
     (r'^slett/(?P<news_id>\d+)/$', 'delete_news'),
 )
