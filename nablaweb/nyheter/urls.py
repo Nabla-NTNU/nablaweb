d40016d72aa32e586d296d1d65a423c252029c53
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-04-17 19:38:02 +0200

    Visning av nyheter er nå under 'nyheter/vis/X/', ikke 'nyheter/X/'.

--- nablaweb/nyheter/urls.py
+++ nablaweb/nyheter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('nyheter.views',
     (r'^$', 'list_news'),
-    (r'^(?P<news_id>\d+)/$', 'show_news'),
+    (r'^vis/(?P<news_id>\d+)/$', 'show_news'),
     (r'^opprett/(?P<news_id>\d+)/$', 'create_news'),
     (r'^endre/(?P<news_id>\d+)/$', 'edit_news'),
     (r'^slett/(?P<news_id>\d+)/$', 'delete_news'),
