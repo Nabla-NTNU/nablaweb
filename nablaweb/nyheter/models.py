fd7f33743ff029c1fc1014cfd673eec843ba7c3c
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-04-23 00:53:44 +0200

    Meta-klassen til News arver Meta-klassen til SiteContent.

--- nablaweb/nyheter/models.py
+++ nablaweb/nyheter/models.py
@@ -3,7 +3,7 @@
 from innhold.models import SiteContent
 
 class News(SiteContent):
-    class Meta:
+    class Meta(SiteContent.Meta):
         verbose_name_plural = "news"
     
     def __unicode__(self):
