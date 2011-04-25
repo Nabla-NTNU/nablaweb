68bcf52d46ec013652dce17cd68e542155e6bbda
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-04-26 01:30:22 +0200

    Opprett og flytt deler av EventForm over i HappeningForm.

--- nablaweb/arrangement/forms.py
+++ nablaweb/arrangement/forms.py
@@ -1,7 +1,7 @@
 # arrangement/forms.py
 
 from django import forms
-from arrangement.models import Event
+from arrangement.models import Event, Happening
 from innhold.forms import SiteContentForm
 import datetime
 
@@ -17,7 +17,7 @@ DATE_FORMATS = ['%Y-%m-%d %H:%M:%S',
 
 DATE_FORMAT = DATE_FORMATS[1]
 
-class EventForm(SiteContentForm):
+class HappeningForm(SiteContentForm):
     event_start = forms.DateTimeField(input_formats=DATE_FORMATS,
                                       widget = forms.DateTimeInput(format=DATE_FORMAT),
                                       required=True,)
@@ -25,11 +25,19 @@ class EventForm(SiteContentForm):
                                     widget = forms.DateTimeInput(format=DATE_FORMAT),
                                     required=False,)
 
+    class Meta(SiteContentForm.Meta):
+        model = Happening
+
+
+class EventForm(SiteContentForm):
     registration_required = forms.BooleanField(required=False)
     deregistration_allowed = forms.BooleanField(required=False)
     has_registration_deadline = forms.BooleanField(required=False)
-
     has_queue = forms.BooleanField(required=False)
 
-    class Meta:
+    class Meta(HappeningForm.Meta):
         model = Event
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        return cleaned_data
