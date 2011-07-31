75261208b3152960631a15ed470b601dd1e874ca
Oyvind Bergland Leknes <oyvinlek@stud.ntnu.no>
2011-07-31 17:17:35 +0200

    Gjør koden for å administrere arrangement litt mer modulær.

--- nablaweb/events/views.py
+++ nablaweb/events/views.py
@@ -2,8 +2,6 @@
 
 
 import datetime
-import re
-from collections import OrderedDict
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
@@ -11,48 +9,66 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, RequestContext, loader
 from django.views.generic import TemplateView
 from nablaweb.content.views import ContentListView, ContentDetailView, ContentDeleteView
-from nablaweb.events.forms import EventForm
 from nablaweb.events.models import Event
 
 
 # Administrasjon
 
-def administer(request, pk):
+def _admin_mov(request, instance):
+    user_list = request.POST.getlist('user')
+    text = request.POST.get('text')
+    try:
+        place = int(text)
+        for user in user_list:
+            user = User.objects.get(username=user)
+            if instance.is_registered(user):
+                instance.move_user_to_place(user, place)
+    except (ValueError, User.DoesNotExist): pass
+_admin_mov.short = 'mov'
+_admin_mov.info = 'Flytt til'
+
+
+def _admin_add(request, instance):
+    text = request.POST.get('text')
+    try:
+        user = User.objects.get(username=text)
+        instance.register_user(user)
+    except User.DoesNotExist: pass
+_admin_add.short = 'add'
+_admin_add.info = 'Legg til'
+
+
+def _admin_del(request, instance):
+    user_list = request.POST.getlist('user')
+    for user in user_list:
+        try:
+            user = User.objects.get(username=user)
+            instance.deregister_user(user)
+        except User.DoesNotExist: pass
+_admin_del.short = 'del'
+_admin_del.info = 'Fjern'
+
+
+def administer(request, pk,
+               actions=(_admin_add, _admin_mov, _admin_del),
+               view='event_admin'):
     event = get_object_or_404(Event, pk=pk)
-    actions = OrderedDict([('mov','Flytt til'),
-                           ('del','Fjern'),
-                           ('add','Legg til'),])
 
     if request.method == 'POST':
-        action = request.POST.get('action')
-        user_list = request.POST.getlist('user')
-        text = request.POST.get('text')
-        if action == 'mov':
-            try:
-                place = int(text)
-                for user in user_list:
-                    user = User.objects.get(username=user)
-                    if event.is_registered(user):
-                        event.move_user_to_place(user, place)
-            except (ValueError, User.DoesNotExist): pass
-        elif action == 'del':
-            for user in user_list:
-                try:
-                    user = User.objects.get(username=user)
-                    event.deregister_user(user)
-                except User.DoesNotExist: pass
-        elif action == 'add':
-            try:
-                user = User.objects.get(username=text)
-                event.register_user(user)
-            except User.DoesNotExist: pass
+        action_name = request.POST.get('action')
+        for action in actions:
+            if action.short == action_name:
+                action(request, event)
+                break
 
         # Unngå at handlingen utføres på nytt dersom brukeren laster siden om igjen
-        return HttpResponseRedirect(reverse('event_admin', kwargs={'pk': pk}))
+        return HttpResponseRedirect(reverse(view, kwargs={'pk': pk}))
 
     registrations = event.eventregistration_set.all().order_by('number')
     return render_to_response('events/event_administer.html',
-                              {'event': event, 'registrations': registrations, 'actions': actions, 'method': 'GET'},
+                              {'event': event,
+                               'registrations': registrations,
+                               'actions': [(a.short, a.info) for a in actions]},
                               context_instance=RequestContext(request))
 
 
