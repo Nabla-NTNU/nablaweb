e6c3e9ebe7f4a1459442005570eeefdc12c1f668
Lars André Kristiansen <lars@larsandre.com>
2011-10-02 15:07:14 +0200

    La til vote-view i poll-appen + .gitignore for mac-spesifikke filer.

--- nablaweb/poll/views.py
+++ nablaweb/poll/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from poll.models import Poll, Choice
+from django.shortcut import get_object_or_404
+from django.http import RequestContext, HttpResponse, HttpResponseRedirect
+
+def vote(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        choice = poll.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return HttpResponse("Du valgte ikke et svaralternativ.")
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('')) # redirect hvor?
