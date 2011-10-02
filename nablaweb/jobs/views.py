73173d377135f0fda6c014e04b8237e8193f5115
Trygve Bertelsen Wiig <trygvwii@stud.ntnu.no>
2011-10-02 15:15:23 +0200

    Fungerende liste over stillingsannonser (haaper jeg)

--- nablaweb/jobs/views.py
+++ nablaweb/jobs/views.py
@@ -4,11 +4,17 @@
 
 from nablaweb.content.views import *
 from nablaweb.jobs.models import Advert, Company
+from django.shortcuts import get_object_or_404
 
-class List(ListView):
+class GenericList(ListView):
     context_object_name = "content_list"
     template_name = "content/content_list.html"
-    
+
+class EverythingList(GenericList):
     def get_queryset(self):
-        company = get_object_or_404(Company, name__iexact=self.args[0])
+        return Advert.objects.filter()
+
+class CompanyList(GenericList):
+    def get_queryset(self):  
+        company = get_object_or_404(Company, headline__iexact=self.kwargs['company'])
         return Advert.objects.filter(company=company)
