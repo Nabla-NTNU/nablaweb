217b197a6c46f91419f89d625fa9c889bcd558dd
Øystein Hiåsen <hiasen@stud.ntnu.no>
2011-10-18 10:00:27 +0200

    Endret språkkoden i globalsettings til norsk bokmål

--- nablaweb/globalsettings.py
+++ nablaweb/globalsettings.py
@@ -39,7 +39,7 @@ TIME_ZONE = 'Europe/Oslo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'no-nb'
 
 SITE_ID = 1
 
