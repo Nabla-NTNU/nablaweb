33158398b4c0cabfc4b96af215f8c4083fe758fb
Andreas Røssland <andreas.roessland@gmail.com>
2011-10-19 20:14:40 +0200

    Lagt til en try/except i accounts/admin.py
    Og lagt ut old_base.html

--- nablaweb/accounts/admin.py
+++ nablaweb/accounts/admin.py
@@ -9,6 +9,11 @@ class GroupProfileInline(admin.StackedInline):
 class ExtendedGroupAdmin(GroupAdmin):
     inlines = GroupAdmin.inlines + [GroupProfileInline]
 
+try:
+    admin.site.unregister(Group)
+except:
+    pass
+
 admin.site.register(Group, ExtendedGroupAdmin)
 
 
@@ -17,5 +22,10 @@ class UserProfileInline(admin.StackedInline):
 
 class ExtendedUserAdmin(UserAdmin):
     inlines = UserAdmin.inlines + [UserProfileInline]
+
+try:
+    admin.site.unregister(User)
+except:
+    pass
 
 admin.site.register(User, ExtendedUserAdmin)
