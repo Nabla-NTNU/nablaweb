900f9d788e7162bdaebc99285163f3f1300cbdc4
Øystein Hiåsen <hiasen@stud.ntnu.no>
2011-10-23 11:45:23 +0200

    Fikset en bug i Quotes sin context processor
    Context processoren tok ikke hensyn til om det ikke var
    noen sitater i databasen. Det er nå fikset.

--- nablaweb/quotes/context_processors.py
+++ nablaweb/quotes/context_processors.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from quotes.models import Quote
 
 def random_quote(request):
-    quote = Quote.objects.order_by('?')[0]
+    try:
+        quote = Quote.objects.order_by('?')[0]
+    except:
+        quote = ""
     return {'random_quote': quote }
