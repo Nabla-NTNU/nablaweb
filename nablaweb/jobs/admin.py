1a28a5baf93fd974018d45d450de316e313db8a4
Trygve Bertelsen Wiig <trygvwii@stud.ntnu.no>
2011-10-27 12:17:20 +0200

    Store endringer i jobs.
    To nye modeller og endringer i eksisterende.

--- nablaweb/jobs/admin.py
+++ nablaweb/jobs/admin.py
@@ -5,23 +5,28 @@
 from django.contrib import admin
 from django.forms import ModelForm
 from django.forms import MultipleChoiceField
-from jobs.models import Advert
-from jobs.models import Company
+from jobs.models import Advert, Company, RelevantForChoices, TagChoices
 
-class RelevantForAdminForm(ModelForm):
-    RELEVANT_FOR_CHOICES = ((u'B', u'Biofysikk'), (u'T', u'Teknisk fysikk'), (u'I', u'Industriell matematikk'))
-    relevant_for = MultipleChoiceField(choices=RELEVANT_FOR_CHOICES)
-    class Meta:
-        model = Advert
+#class RelevantForAdminForm(ModelForm):
+#    RELEVANT_FOR_CHOICES = ((u'B', u'Biofysikk'), (u'T', u'Teknisk fysikk'), (u'I', u'Industriell matematikk'))
+#    relevant_for = MultipleChoiceField(choices=RELEVANT_FOR_CHOICES)
+#    class Meta:
+#        model = Advert
 
 class AdvertAdmin(admin.ModelAdmin):
-    relevant_for_form = RelevantForAdminForm
-    class Meta:
-        verbose_name_plural = "Adverts"
+#    relevant_for_form = RelevantForAdminForm
+    pass
     
 class CompanyAdmin(admin.ModelAdmin):
-    class Meta:
-        verbose_name_plural = "Companies"
+    pass
+        
+class RelevantForChoicesAdmin(admin.ModelAdmin):
+    pass
+    
+class TagChoicesAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Advert, AdvertAdmin)
 admin.site.register(Company, CompanyAdmin)
+admin.site.register(RelevantForChoices, RelevantForChoicesAdmin)
+admin.site.register(TagChoices, TagChoicesAdmin)
