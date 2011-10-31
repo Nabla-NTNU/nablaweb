57afc33ec4eb1dc282b129c4193fed1aa51bd33a
Trygve Bertelsen Wiig <trygvwii@stud.ntnu.no>
2011-10-31 13:45:33 +0100

    Startet på en IRC-app

---
+++ nablaweb/irc/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Views for irc-appen
+
+from irc.models import *
+from django.template import Context, RequestContext, loader
+
+def ShowChannel(request, pk):
+    pass
+    
+def ShowChannelStats(request, pk):
+    pass
+
+def ShowTotalStats(request):
+    pass
