592efebdcbba2c1209657bf187c98b34944837d5
Andreas Røssland <andreas.roessland@gmail.com>
2012-02-25 23:33:40 +0100

    Lagt til bootstrap-src, ryddet, endret navbar-css
    * Puttet twitter-bootstrap i dependencies. Valgte å ikke bruke
      git-submodules, siden jeg ikke har peiling på det.
    
      For å kompilere .less-filene, kjør "make" i bootstrap-mappen.
      Guide: http://twitter.github.com/bootstrap/less.html
    
    * Slettet gammel jQuery, slettet jQuery UI.
    
    * Slettet innehold i media bundles. Unødvendig før production.

--- nablaweb/media_bundles.py
+++ nablaweb/media_bundles.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 MEDIA_BUNDLES = (
     ('all.css',
-        'css/redmond/jquery-ui-1.8.16.custom.css', 
     ),
 
     ('all.js',
-        'js/jquery-1.6.2.min.js',
-        'js/jquery-ui-1.8.16.custom.min.js',
-        'js/jquery.ui.datepicker-no.js',
     ),
 )
