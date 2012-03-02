d31c3c0bdca7ca7ea2a516f807030351970797b9
Øystein Hi"åsen <hiasen@gauss.nt.ntnu.no>
2012-03-02 13:51:16 +0100

    Lagt til endring av medlemmer i gruppeadmin

--- nablaweb/accounts/admin.py
+++ nablaweb/accounts/admin.py
@@ -1,12 +1,47 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from accounts.models import GroupProfile, UserProfile
+
+
+
+class GroupAdminForm(forms.ModelForm):
+    users = forms.ModelMultipleChoiceField(queryset=User.objects.all(),
+                                           widget=FilteredSelectMultiple('Users', False),
+                                           required=False)
+    class Meta:
+        model = Group
+        
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance', None)
+        if instance is not None:
+            initial = kwargs.get('initial', {})
+            initial['users'] = instance.user_set.all()
+            kwargs['initial'] = initial
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        
+    def save(self, commit=True):
+        group = super(forms.ModelForm, self).save(commit=commit)
+        
+        if commit:
+            group.user_set = self.cleaned_data['users']
+        else:
+            old_save_m2m = self.save_m2m
+            def new_save_m2m():
+                old_save_m2m()
+                group.user_set = self.cleaned_data['users']
+            self.save_m2m = new_save_m2m
+        return group
+
 
 class GroupProfileInline(admin.StackedInline):
     model = GroupProfile
 
 class ExtendedGroupAdmin(GroupAdmin):
+    form = GroupAdminForm
     inlines = GroupAdmin.inlines + [GroupProfileInline]
 
 try:
