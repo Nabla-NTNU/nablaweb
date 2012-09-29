1bfccb6a3ac910896fe8f2a51c029ac58cc9bdf1
Andreas Røssland <andreas.roessland@gmail.com>
2012-09-29 03:05:25 +0200

    Lagt til en utkommentert memcache til CACHE_BACKEND

--- nablaweb/globalsettings.py
+++ nablaweb/globalsettings.py
@@ -41,6 +41,8 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        #'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        #'LOCATION': '127.0.0.1:11211'
     }
 }
 
