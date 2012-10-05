19e1b24444a55f8e43c89c63a6dc5db21f95eea1
Øystein Hiåsen <hiasen@stud.ntnu.no>
2012-10-05 15:17:55 +0200

    Språkkoden i globalsettings var feil. Endretw fra no-nb til nb.

--- nablaweb/globalsettings.py
+++ nablaweb/globalsettings.py
@@ -55,7 +55,7 @@ TIME_ZONE = 'Europe/Oslo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'no-nb'
+LANGUAGE_CODE = 'nb'
 
 SITE_ID = 1
 
