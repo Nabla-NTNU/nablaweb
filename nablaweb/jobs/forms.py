e5c1e118bba05ff9f1444c63a0eac2cdfab34caa
Trygve Bertelsen Wiig <trygvwii@stud.ntnu.no>
2013-02-24 18:33:51 +0100

    Glemte å commite forms.py i jobs.

---
+++ nablaweb/jobs/forms.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.forms import ModelForm
+from nablaweb.jobs.models import Advert, Company
+
+class AdvertForm(ModelForm):
+    headline = forms.CharField(help_text="Tittelen på stillingsannonsen. Bør inneholde bedriftens navn")
+    class Meta:
+        model = Advert
+
+class CompanyForm(ModelForm):
+    headline = forms.CharField(help_text="Navnet på bedriften")
+    picture = forms.FileField(help_text="Bilder som er større enn 770x250 px ser best ut. Du kan beskjære bildet etter opplasting.", label="Logo", required=False)
+    class Meta:
+        model = Company
