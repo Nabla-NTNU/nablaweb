bc1713f952e8010b3e26fbaa3b67fbcc1cf15f45
Øystein Hiåsen <hiasen@stud.ntnu.no>
2013-05-01 20:01:24 +0200

    Lagt til nyeste xkcd i sidebaren.
    Jeg liker xkcd, derfor bør alle andre fysmatter like det og. :)

--- nablaweb/context_processors.py
+++ nablaweb/context_processors.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+import urllib
+import json
+
 def primary_dir(request):
     """Adds the primary URL path to context.
 
@@ -12,3 +16,20 @@ def primary_dir(request):
         primary_dir_slashes = '/'
     return {'primary_dir': primary_dir,
             'primary_dir_slashes': primary_dir_slashes }
+
+
+def xkcd(request):
+    """Gets info about the newest xkcd-comic.
+
+    """
+    uopen = urllib.urlopen('http://xkcd.com/info.0.json')
+    json_string = uopen.read()
+    d = json.loads(json_string)
+    # Stoler på at xkcd ikke har noe tull(javascript ol.)  her, men burde egentlig sikres på
+    # en eller annen måte.
+    context = {}
+    keys = ['safe_title','alt','img','title']
+    for key in keys:
+        context['xkcd_'+key] = d[key]
+    return context
+
