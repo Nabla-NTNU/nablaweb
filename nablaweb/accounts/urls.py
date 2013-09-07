a0fba59116dc7e6ae90d641fe6b736978ae180ac
Andreas Røssland <andreas.roessland@gmail.com>
2013-09-07 13:31:39 +0200

    Gjør det mulig å endre passord

--- nablaweb/accounts/urls.py
+++ nablaweb/accounts/urls.py
@@ -3,7 +3,9 @@
 from django.conf.urls.defaults import *
 from django.views.generic import RedirectView
 
-urlpatterns = patterns('django.contrib.auth.views',
+urlpatterns = patterns('django.contrib.auth.views', 
+    url(r'password/change/$', 'password_change', name='password_change'),
+    url(r'password/change/done$', 'password_change_done', name='password_change_done'),
     (r'password/reset/$', 'password_reset'),
     (r'password/reset/done/$', 'password_reset_done'),
     (r'password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm'),
