7fa728efb6cc664312d619d9982816fbb937754c
Andreas Røssland <andreas.roessland@gmail.com>
2014-03-24 21:39:58 +0100

    bedkomnabla

--- nablaweb/events/context_processors.py
+++ nablaweb/events/context_processors.py
@@ -7,12 +7,12 @@ from itertools import chain
 import calendar
 
 def upcoming_events(request):
-    """Legger globalt til en template-variabel upcoming_events"""
+    """Legger globalt til en template-variabel upcoming_events, og upcoming_bedpresses"""
     now = datetime.now()
     upcoming_events = Event.objects.filter(event_start__gte=now).order_by('event_start')[:6]
     upcoming_bedpresses = BedPres.objects.filter(event_start__gte=now).order_by('event_start')[:6]
     upcoming = sorted(chain(upcoming_events,upcoming_bedpresses),cmp=lambda e1,e2: 1 if e1.event_start>e2.event_start else  -1)[:6]
-    return {'upcoming_events': upcoming}
+    return {'upcoming_events': upcoming, 'upcoming_bedpresses': upcoming_bedpresses}
 
 class Day(object):
     def __init__(self, arr, day):
@@ -25,6 +25,7 @@ class Day(object):
         return __unicode__(self)
 
 
+# TODO: Blir denne brukt noe sted?
 def current_month_calendar(request):
     today = date.today()
     month = today.month
