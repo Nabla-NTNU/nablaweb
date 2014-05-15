95fa89c8fa8f730ac639652912e6ec6b60d1bd05
Ane Hov <anenhov@gmail.com>
2014-05-16 00:59:08 +0200

    Lager en oversiktsside for Skraattcast.

--- nablaweb/skraattcast/urls.py
+++ nablaweb/skraattcast/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from skraattcast import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='skraattcast_index')
 ]
 
--- nablaweb/skraattcast/views.py
+++ nablaweb/skraattcast/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Dette er en test.")
+    return render(request, 'skraattcast/index.html', {})
 
