8209ed9f9c8114be45fc3859b4398e195d5d96f7
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2014-07-14 10:37:15 +0200

    Fikset at poll gjorde veldig sql-forespørsler.
    Synderen var at Poll.get_total_votes ble kjørt to ganger for hvert choice i
    poll_include.html
    Flyttet det til context_processors.py

--- nablaweb/poll/context_processors.py
+++ nablaweb/poll/context_processors.py
@@ -8,6 +8,7 @@ def poll_context(request):
         poll = Poll.objects.filter(is_current=True)[0]
         context['poll'] = poll
         context['poll_has_voted'] = poll.user_has_voted(request.user)
+        context['poll_total_votes'] = poll.get_total_votes()
     except:
         pass
     return context
