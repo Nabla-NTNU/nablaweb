7e9b1f12969086963bde0223079a6355e95e0a2a
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2014-07-22 18:17:20 +0200

    Lagt til feltene fra UserProfile til NablaUser med migrations

--- nablaweb/accounts/models.py
+++ nablaweb/accounts/models.py
@@ -5,7 +5,16 @@ from django.contrib.auth.models import Group, AbstractUser
 from datetime import date
 
 class NablaUser(AbstractUser):
-    pass
+    telephone = models.CharField("Telefon", max_length = 15, blank=True)
+    cell_phone = models.CharField("Mobil", max_length = 15, blank=True)
+    birthday = models.DateField("Bursdag", blank =True, null=True)
+    address = models.CharField("Adresse", max_length = 40, blank = True)
+    mail_number = models.CharField("Postnr", max_length = 4, blank = True)
+    web_page = models.CharField("Hjemmeside", max_length = 80, blank = True)
+    wants_email = models.BooleanField("Motta kullmail", default = True)
+    about = models.TextField("Biografi",blank = True)
+    avatar = models.ImageField('Avatar', blank=True, null=True, upload_to='avatars')
+    ntnu_card_number = models.CharField("NTNU kortnr",max_length = 10, blank = True, help_text ="Dette er det 7-10 siffer lange nummeret <b>nede til venstre</b> på baksiden av NTNU-adgangskortet ditt. Det brukes blant annet for å komme inn på bedpresser.")
 
 
 class UserProfile(models.Model):
