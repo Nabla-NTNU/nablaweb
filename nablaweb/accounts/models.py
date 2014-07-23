b5fc62e50baec30541df7e70577f727da9533657
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2014-07-23 16:35:31 +0200

    Fjernet UserProfile
    Userprofile er nå fjernet, men jeg har ikke skrevet noen migrasjoner som
    sletter database-innholdet til UserProfile, sånn i tilfelle noe går galt.

--- nablaweb/accounts/models.py
+++ nablaweb/accounts/models.py
@@ -29,31 +29,6 @@ class NablaUser(AbstractUser):
         return reverse("member_profile", kwargs={"username": self.username})
 
 
-class UserProfile(models.Model):
-    """ Ekstrainformasjon for brukere. """
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    telephone = models.CharField("Telefon", max_length = 15, blank=True)
-    cell_phone = models.CharField("Mobil", max_length = 15, blank=True)
-    birthday = models.DateField("Bursdag", blank =True, null=True)
-    address = models.CharField("Adresse", max_length = 40, blank = True)
-    mail_number = models.CharField("Postnr", max_length = 4, blank = True)
-    web_page = models.CharField("Hjemmeside", max_length = 80, blank = True)
-    wants_email = models.BooleanField("Motta kullmail", default = True)
-    about = models.TextField("Biografi",blank = True)
-    avatar = models.ImageField('Avatar', blank=True, null=True, upload_to='avatars')
-    ntnu_card_number = models.CharField("NTNU kortnr",max_length = 10, blank = True, help_text ="Dette er det 7-10 siffer lange nummeret <b>nede til venstre</b> på baksiden av NTNU-adgangskortet ditt. Det brukes blant annet for å komme inn på bedpresser.")
-
-    def get_class_number(self):
-        """ Henter hvilken klasse på fysmat (1-5) brukeren går i. Returnerer 0 hvis brukeren ikke går på fysmat."""
-        try:
-            return FysmatClass.objects.filter(user = self.user).order_by('starting_year')[0].get_class_number()
-        except:
-            return 0
-
-    def __unicode__(self):
-        return "< %s profile >" % self.user.username
-
-
 class NablaGroup(Group):
     """
     Subklasse av Group som definerer ekstrainformasjon om grupper
