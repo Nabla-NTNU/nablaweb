3a1557b32fb9beb27ec7cf512cffcc9584f36620
Trygve Bertelsen Wiig <trygvebw@gmail.com>
2015-02-13 01:07:02 +0100

    La til flere leservennlige navn på apper

---
+++ nablaweb/events/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+class EventsConfig(AppConfig):
+    name = 'events'
+    verbose_name = 'Arrangementer'
---
+++ nablaweb/poll/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+class PollConfig(AppConfig):
+    name = 'poll'
+    verbose_name = 'Avstemninger'
