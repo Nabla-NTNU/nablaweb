7bbf38d5cfa0d633fb9885e872ab0b3472bb705f
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2015-02-14 22:00:36 +0100

    Flyttet brukeraktiveringsrutinene ut av aktiveringsviewet

--- nablaweb/accounts/views.py
+++ nablaweb/accounts/views.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import DetailView, ListView, UpdateView, FormView
-from django.template import loader, Context
 from django.contrib import messages
 from django.contrib.messages.api import MessageFailure
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import UserManager
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 from braces.views import LoginRequiredMixin
 
 from .forms import UserForm, RegistrationForm
 from .models import NablaUser
+from .utils import activate_user_and_create_password, send_activation_email
 
 User = get_user_model()
 
@@ -60,30 +59,12 @@ class RegistrationView(FormView):
         username = form.cleaned_data['username'] 
         user, created_user = NablaUser.objects.get_or_create(username=username)
 
-        password = self._activate_user_and_create_password(user)
-        self._send_activation_email(user, password)
+        password = activate_user_and_create_password(user)
+        send_activation_email(user, password)
 
         messages.add_message(self.request,messages.INFO,
                              'Registreringsepost sendt til %s' % user.email)
         return super(RegistrationView, self).form_valid(form)
-
-    def _activate_user_and_create_password(self, user):
-        studmail = user.username+"@stud.ntnu.no"
-        if not(user.email):
-            user.email = studmail
-
-        user_manager = UserManager()
-        password = user_manager.make_random_password()
-        user.set_password(password)
-        user.is_active = True
-        user.save()
-        return password
-
-    def _send_activation_email(self, user, password):
-        t = loader.get_template('accounts/registration_email.txt')
-        email_text = t.render(Context({"username": user.username,
-                                       "password": password}))
-        user.email_user('Bruker på nabla.no', email_text)
 
 
 ## Login/logout meldinger
