bfe0887a5ea5f31d2ae1885dff39976bcdb8566d
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2015-03-27 13:54:18 +0100

    Ryddet opp + pep8 fix i nablad-appen

--- nablaweb/nabladet/urls.py
+++ nablaweb/nabladet/urls.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import *
+from django.conf.urls import patterns, url
 from nabladet.views import NabladListView, NabladDetailView
 
-urlpatterns = patterns('nablad.views',
-    # Offentlig
-    url(r'^$', 
-        NabladListView.as_view( context_object_name = "nablad_list" ),
+urlpatterns = patterns('',
+    url(r'^$',
+        NabladListView.as_view(),
         name='nablad_list'),
-    url(r'^(?P<pk>\d{1,8})/(?P<slug>[-\w]*)$', 
-        NabladDetailView.as_view( context_object_name = "nablad" ),
+    url(r'^(?P<pk>\d{1,8})/(?P<slug>[-\w]*)$',
+        NabladDetailView.as_view(),
         name='nablad_detail'),
 )
