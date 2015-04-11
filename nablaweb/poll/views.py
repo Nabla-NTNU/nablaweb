3eb84c475a2b5a52344e7d4e7067f170621fae99
Anders Christiansen Sørby <user8715@gmail.com>
2015-04-11 12:53:54 +0200

    La til en oversikt over antall stemmer i poll.
    Endret noen PEP8-feil.

--- nablaweb/poll/views.py
+++ nablaweb/poll/views.py
@@ -2,9 +2,9 @@
 
 from poll.models import Poll, Choice
 from django.shortcuts import get_object_or_404, redirect
-from django.template import RequestContext
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def vote(request, poll_id):
