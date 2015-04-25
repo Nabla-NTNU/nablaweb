998da03e8409534cb45a3c94d058a2b3f008087f
Anders Christiansen Sørby <user8715@gmail.com>
2015-04-25 13:58:48 +0200

    La til django-filbrowser.
    Dette åpner for mange nye muligheter samt en mye penere adminside.

--- nablaweb/nablaweb/urls.py
+++ nablaweb/nablaweb/urls.py
@@ -9,11 +9,18 @@ from news.feeds import RecentNews
 from wiki.urls import get_pattern as get_wiki_pattern
 from django_nyt.urls import get_pattern as get_nyt_pattern
 
+from filebrowser.sites import site
 
+# Denne må før admin
+urlpatterns = patterns('',
+   (r'^admin/filebrowser/', include(site.urls)),
+   (r'^grappelli/', include('grappelli.urls')),
+   (r'^admin/', include(admin.site.urls)),
+)
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns += patterns('',
     (r'^$', include('news.urls')),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='auth_login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
