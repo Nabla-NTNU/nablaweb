2abc7747b55d6279dba22477db49fca1118334f6
Anders Christiansen Sørby <user8715@gmail.com>
2015-05-23 15:47:28 +0200

    Fikset autenisering og synlighet for album.

--- nablaweb/content/urls.py
+++ nablaweb/content/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import AlbumOverview, AlbumView
 
 urlpatterns = patterns('content.views',
-    url(r'^albums',
+    url(r'^album/$',
         AlbumOverview.as_view(),
         name='albums'),
     url(r'^album/(?P<pk>\d{1,8})/(?P<num>\d{1,8})',
