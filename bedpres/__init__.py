08207fae63deaff4fc2b84243b82a53861e9759c
Anders Christiansen Sørby <user8715@gmail.com>
2015-09-02 07:35:36 +0200

    Added an overriding method for bedpres

--- bedpres/__init__.py
+++ bedpres/__init__.py
@@ -1,1 +1,2 @@
 default_app_config = 'bedpres.apps.BedpresConfig'
+
--- nablaweb/context_processors.py
+++ nablaweb/context_processors.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from content.models import Event
+from bedpres.models import BedPres
+from datetime import datetime
+from itertools import chain
+from operator import attrgetter
 
 
 def get_primary_dir(request):
@@ -15,3 +20,12 @@ def get_primary_dir(request):
         primary_dir_slashes = '/'
     return {'primary_dir': primary_dir,
             'primary_dir_slashes': primary_dir_slashes}
+
+
+def upcoming_events(request):
+    """Legger globalt til en template-variabel upcoming_events"""
+    now = datetime.now()
+    upcoming_events = Event.objects.filter(event_start__gte=now).order_by('event_start')[:6]
+    upcoming_bedpresses = BedPres.objects.filter(event_start__gte=now).order_by('event_start')[:6]
+    upcoming = sorted(chain(upcoming_events), key=attrgetter("event_start"))[:6]
+    return {'upcoming_events': upcoming}
