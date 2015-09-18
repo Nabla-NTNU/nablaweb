83e9127b21d237e08918a7837e541ede6d16dc86
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2015-09-18 19:44:59 +0200

    Legger til mer logging av bruk av bpc dummy data

--- bedpres/bpcmixin.py
+++ bedpres/bpcmixin.py
@@ -42,10 +42,15 @@ class BPCEventMixin(object):
         except BPCConnectionError:
             event.data = self._dummy_data
             logger = logging.getLogger(__name__)
-            logger.warning("No connection to BPC. Used dummy data instead.")
+            logger.error("No connection to BPC. Used dummy data instead.")
         except BPCResponseException as e:
             if e.bpc_error_code == '403' or e.bpc_error_code == '103':
                 event.data = self._dummy_data
+                logger = logging.getLogger(__name__)
+                logger.error("BPC doesn't have an event with id {}. "
+                             "Used dummy data instead.".format(self.bpcid))
+            else:
+                raise e
         return event
 
     def register_user(self, user):
