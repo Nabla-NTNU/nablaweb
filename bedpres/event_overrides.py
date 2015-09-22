e291f3ac23c6659ab4ed0a25cb283a63dc36f504
Anders Christiansen Sørby <user8715@gmail.com>
2015-09-22 11:15:09 +0200

    Fikset en bug.

--- bedpres/event_overrides.py
+++ bedpres/event_overrides.py
@@ -1,8 +1,9 @@
 from bedpres.models import BedPres
 from content.models import Event
 
-from content.views.events import set_current_events
+from content.views.events import set_current_events, set_get_event
 from itertools import chain
+from django.shortcuts import get_object_or_404
 
 
 def get_current_events(year, month):
@@ -18,3 +19,14 @@ def get_current_events(year, month):
     return chain(events, bedpress)
 
 set_current_events(get_current_events, True)
+
+
+def get_event(event_id):
+    # Try Event first, then BedPres. 404 if none of them are found.
+    try:
+        return Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        return get_object_or_404(BedPres, pk=event_id)
+
+
+set_get_event(get_event, True)
