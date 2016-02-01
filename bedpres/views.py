56f77a7e50d00ef4ee0e44779d7cca4730fa701d
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-02-01 20:23:50 +0100

    Bedpres: Fikser feil kullbug
    Siden krasjer ikke lenger hvis man er i feil kull for å melde seg på en
    bedpres. Man får heller en informativ feilmelding.

--- bedpres/views.py
+++ bedpres/views.py
@@ -3,6 +3,7 @@
 from content.views import EventDetailView, RegisterUserView
 from .event_overrides import *
 from .utils import InvalidCardNum
+from .bpcmixin import WrongClass
 
 
 class BedPresRegisterUserView(RegisterUserView):
@@ -13,6 +14,17 @@ class BedPresRegisterUserView(RegisterUserView):
             super().register_user(user)
         except InvalidCardNum:
             return "Du ble ikke påmeldt fordi du mangler gyldig NTNU-kortnummer."
+        except WrongClass as e:
+            if e.user.get_class_number() == 0:
+                return (
+                    "Du ble ikke påmeldt fordi du ikke er registrert på noe kull. "
+                    "Send en epost til webkom@nabla.ntnu.no for å fikse det.")
+            else:
+                return (
+                    "Du ble ikke påmeldt fordi du går i feil klasse for dette arrangementet.\n"
+                    "I følge våre systemer går du i {klasse}. klasse og arrangementet "
+                    "er kun åpent for {event.bpc_event.min_year}. til {event.bpc_event.max_year}. klasse."
+                ).format(klasse=e.user.get_class_number(), event=e.event)
 
 
 class BedPresDetailView(EventDetailView):
