8ec670083c3f91d8aab86c87b4dfd657e1dd75d6
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-03-02 15:33:51 +0100

    SimpleTestCase.urls -> @override_settings
    Fixes deprecation warning when running tests.
    
    RemovedInDjango110Warning: SimpleTestCase.urls is deprecated and will be
    removed in Django 1.10. Use @override_settings(ROOT_URLCONF=...) in
    RecentEventsTestCase instead.

--- content/tests/events/test_feed.py
+++ content/tests/events/test_feed.py
@@ -1,11 +1,11 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, override_settings
 from django.core.urlresolvers import reverse
 
 from content.models.events import Event
 
 
+@override_settings(ROOT_URLCONF="content.urls")
 class RecentEventsTestCase(TestCase):
-    urls = "content.urls"
 
     def test_feed(self):
         events = [Event.objects.create(headline="Event{}".format(i), lead_paragraph="Yo{}".format(i)) for i in range(10)]
