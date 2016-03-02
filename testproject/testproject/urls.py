2e1ccb49ac93f3713f46a272a68585f1768b4bee
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-03-02 15:19:13 +0100

    Remove string view arguments to url in testproject
    Fixes this deprecation warning:
    RemovedInDjango110Warning: Support for string view arguments to url() is
    deprecated and will be removed in Django 1.10 (got
    django.contrib.auth.views.login). Pass the callable instead.

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 from django.views.generic import RedirectView
 from content.feeds.news import RecentNews
 import content.urls
@@ -9,10 +10,10 @@ from django_nyt.urls import get_pattern as get_nyt_pattern
 
 urlpatterns = [
     url(r'^login/$',
-        'django.contrib.auth.views.login',
+        login,
         {'template_name': 'admin/login.html'}
         ),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', logout),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(content.urls)),
     url(r'^feed/$', RecentNews()),
