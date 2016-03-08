f9476ba89ad7ad484eb9f1527c817ab584020445
Kristoffer Engedal Andreassen <kristoea@stud.ntnu.no>
2016-03-08 22:09:26 +0100

    Legger tilbake <strong> i login-meldingen
    Skikkelig trygg denne gangen

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,16 +3,20 @@ from contextlib import suppress
 from django.contrib import messages
 from django.contrib.messages.api import MessageFailure
 from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
 
 
 def login_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
-        messages.info(request, u'Velkommen inn {user.username}'.format(user=user))
+        msg = 'Velkommen inn <strong>' + escape(user.username) + '</strong>'
+        messages.info(request, mark_safe(msg))
 
 
 def logout_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
-        messages.info(request, u'{user.username} ble logget ut'.format(user=user))
+        msg  = '<strong>' + escape(user.username) + '</strong> ble logget ut'
+        messages.info(request, mark_safe(msg))
 
 
 def register_signals():
