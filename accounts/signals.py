b053e362e46ac3d5eee5ccd7edb842a11c6b2ff9
Kristoffer Engedal Andreassen <kristoea@stud.ntnu.no>
2016-03-08 21:23:32 +0100

    Fjerner potensiell XSS-sikkerhetshull

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,17 +3,16 @@ from contextlib import suppress
 from django.contrib import messages
 from django.contrib.messages.api import MessageFailure
 from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.utils.safestring import mark_safe
 
 
 def login_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
-        messages.info(request, mark_safe(u'Velkommen inn <strong>{user.username}</strong>'.format(user=user)))
+        messages.info(request, u'Velkommen inn {user.username}'.format(user=user))
 
 
 def logout_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
-        messages.info(request, mark_safe(u'<strong>{user.username}</strong> ble logget ut'.format(user=user)))
+        messages.info(request, u'{user.username} ble logget ut'.format(user=user))
 
 
 def register_signals():
