e691828ede47d753fd1da174f71d4345ccac05a6
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-04-03 16:08:30 +0200

    Add check for content_type in NewsDetailView
    and redirects to the correct view.
    Fixes the bug where people use the news-url and not the event url and
    then can't attend the event.

--- content/views/news.py
+++ content/views/news.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.contenttypes.models import ContentType
 from django.views.generic import DetailView, ListView
+from django.shortcuts import redirect
 
 from ..templatetags import listutil
 from ..models.news import News
 from .mixins import AdminLinksMixin, ViewAddMixin, PublishedListMixin, PublishedMixin
+
+
+class WrongContentType(Exception):
+    def __init__(self, object):
+        self.object = object
 
 
 class NewsListView(PublishedListMixin, ListView):
@@ -19,3 +26,15 @@ class NewsDetailView(PublishedMixin, ViewAddMixin, AdminLinksMixin, DetailView):
     model = News
     context_object_name = 'news'
     template_name = 'content/news/news_detail.html'
+
+    def get_object(self):
+        object = super().get_object()
+        if object.content_type != ContentType.objects.get_for_model(self.model):
+            raise WrongContentType(object)
+        return object
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            return super().dispatch(request, *args, **kwargs)
+        except WrongContentType as e:
+            return redirect(e.object)
