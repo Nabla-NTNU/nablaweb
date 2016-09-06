a8f827a30858a4fd1a154401b9273b5506b8b62c
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-09-06 15:07:55 +0200

    Laget migrasjonene på nytt.
    Det beste er å ha så få migrasjoner som overhode mulig.
    For ellers går det så tregt å sette opp utviklingsdatabasen.

--- exchange/migrations/0001_initial.py
+++ exchange/migrations/0001_initial.py
@@ -16,33 +16,42 @@ class Migration(migrations.Migration):
             name='Exchange',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('retning', models.CharField(help_text='Retning', choices=[('Biofysikk og medisinteknologi', 'biofys'), ('Industriell matematikk', 'indmat'), ('Teknisk fysikk', 'tekfys')], max_length=30)),
+                ('retning', models.CharField(max_length=6, help_text='Retning', choices=[('biofys', 'Biofysikk og medisinteknologi'), ('indmat', 'Industriell matematikk'), ('tekfys', 'Teknisk fysikk')])),
                 ('start', models.DateField(help_text='Dato utveksling startet')),
                 ('end', models.DateField(help_text='Dato utveksling sluttet')),
                 ('student', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ['student'],
+                'verbose_name_plural': 'utvekslinger',
+                'verbose_name': ('utveksling',),
             },
         ),
         migrations.CreateModel(
             name='Info',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('title', models.CharField(help_text='Tittelen til innholdet', default='', max_length=50, verbose_name='tittel')),
-                ('body', models.TextField(help_text='Man kan her bruke <a href="http://en.wikipedia.org/wiki/Markdown" target="_blank">markdown</a> for å formatere teksten.', verbose_name='brødtekst', blank=True)),
+                ('title', models.CharField(default='', max_length=50, help_text='Tittelen til innholdet', verbose_name='tittel')),
+                ('body', models.TextField(blank=True, help_text='Man kan her bruke <a href="http://en.wikipedia.org/wiki/Markdown" target="_blank">markdown</a> for å formatere teksten.', verbose_name='brødtekst')),
                 ('ex', models.ForeignKey(to='exchange.Exchange')),
             ],
+            options={
+                'verbose_name_plural': 'info',
+                'verbose_name': ('info',),
+            },
         ),
         migrations.CreateModel(
             name='University',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('univ_navn', models.CharField(help_text='Navnet til universitetet', default='', max_length=50, verbose_name='universitets navn')),
-                ('land', models.CharField(help_text='Landet universitetet ligger i', default='', max_length=30, verbose_name='land')),
+                ('univ_navn', models.CharField(default='', max_length=50, help_text='Navnet til universitetet', verbose_name='universitets navn')),
+                ('desc', models.TextField(blank=True, help_text='En kort beskrivelse av universitetet. Valgfritt.', verbose_name='beskrivelse')),
+                ('land', models.CharField(default='', max_length=30, help_text='Landet universitetet ligger i', verbose_name='land')),
             ],
             options={
                 'ordering': ['univ_navn'],
+                'verbose_name_plural': 'universiteter',
+                'verbose_name': 'universitet',
             },
         ),
         migrations.AddField(
