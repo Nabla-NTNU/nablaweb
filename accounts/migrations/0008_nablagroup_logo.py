86fa12af6182d84f8faddc90c2eea50a47e538fb
Andreas Bolstad <andreasblstd@gmail.com>
2016-09-28 20:46:09 +0200

    Add logos to NablaGroup

---
+++ accounts/migrations/0008_nablagroup_logo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0007_move_to_new_likepress_app'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='nablagroup',
+            name='logo',
+            field=models.FileField(blank=True, upload_to='logos', verbose_name='Logo', null=True),
+        ),
+    ]
