63447344fc030aaaf3b213a5a69004dc23a2ffd0
Anders Christiansen Sørby <user8715@gmail.com>
2016-09-30 16:54:16 +0200

    Fix indent

--- nablaweb/management/commands/seed.py
+++ nablaweb/management/commands/seed.py
@@ -20,120 +20,120 @@ def s():
 
 
 class Command(BaseCommand):
-        help = 'Puts data into the database'
+    help = 'Puts data into the database'
 
-        def add_arguments(self, parser):
-            parser.add_argument(
-                '--delete',
-                dest='delete',
-                default=True,
-                help='Delete existing data',
-            )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            dest='delete',
+            default=True,
+            help='Delete existing data',
+        )
 
-        def handle(self, *args, **options):
-            
-            if not settings.DEBUG:
-                raise Exception("Trying to seed in production.")
-            
-            delete = options['delete']
-            print("Deleting old entries: " + str(delete))
-            
-            if delete:
-                User.objects.all().delete()
-            
-            print("Creating superuser admin")
-            admin = User.objects.create_user(
-                    username='admin',
-                    password='admin',
+    def handle(self, *args, **options):
+        
+        if not settings.DEBUG:
+            raise Exception("Trying to seed in production.")
+        
+        delete = options['delete']
+        print("Deleting old entries: " + str(delete))
+        
+        if delete:
+            User.objects.all().delete()
+        
+        print("Creating superuser admin")
+        admin = User.objects.create_user(
+                username='admin',
+                password='admin',
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                address=fake.address(),
+                email='admin@stud.ntnu.no',
+                about=fake.text(),
+                birthday=fake.date_time_between_dates(datetime_start=None, datetime_end=None, tzinfo=None),
+                )
+        admin.is_superuser = True
+        admin.is_staff = True
+        admin.save()
+
+        if delete:
+            FysmatClass.objects.all().delete()
+
+        count = random.randint(7, 10)
+        print("Creating %d FysmatClasses" % count)
+        year = dt.now().year
+        classes = []
+        for i in range(count):
+            classes.append(FysmatClass.objects.create(
+                starting_year=year-i,
+                name="kull%d" % (year-i)
+            ))
+        
+        if delete:
+            NablaGroup.objects.all().delete()
+
+        count = random.randint(5, 10)
+        print("Creating %d NablaGroups" % count)
+        year = dt.now().year
+        ngroups = []
+        for i in range(count):
+            ngroups.append(NablaGroup.objects.create(
+                name=fake.word()+"-komitéen"
+            ))
+        
+        count = random.randint(50, 100)
+        print("Creating %d NablaUsers" % count)
+        for i in range(count):
+            username = "komponent%d" % (i)
+            groups = [
+                random.choice(ngroups),
+                random.choice(classes)
+            ]
+            user = User.objects.create_user(
+                    username=username,
                     first_name=fake.first_name(),
                     last_name=fake.last_name(),
                     address=fake.address(),
-                    email='admin@stud.ntnu.no',
+                    password='password',
+                    email=username+'@stud.ntnu.no',
                     about=fake.text(),
                     birthday=fake.date_time_between_dates(datetime_start=None, datetime_end=None, tzinfo=None),
-                    )
-            admin.is_superuser = True
-            admin.is_staff = True
-            admin.save()
-
-            if delete:
-                FysmatClass.objects.all().delete()
-
-            count = random.randint(7, 10)
-            print("Creating %d FysmatClasses" % count)
-            year = dt.now().year
-            classes = []
-            for i in range(count):
-                classes.append(FysmatClass.objects.create(
-                    starting_year=year-i,
-                    name="kull%d" % (year-i)
-                ))
+            )
             
-            if delete:
-                NablaGroup.objects.all().delete()
+            user.groups.add(*groups)
+            user.save()
 
-            count = random.randint(5, 10)
-            print("Creating %d NablaGroups" % count)
-            year = dt.now().year
-            ngroups = []
-            for i in range(count):
-                ngroups.append(NablaGroup.objects.create(
-                    name=fake.word()+"-komitéen"
-                ))
-            
-            count = random.randint(50, 100)
-            print("Creating %d NablaUsers" % count)
-            for i in range(count):
-                username = "komponent%d" % (i)
-                groups = [
-                    random.choice(ngroups),
-                    random.choice(classes)
-                ]
-                user = User.objects.create_user(
-                        username=username,
-                        first_name=fake.first_name(),
-                        last_name=fake.last_name(),
-                        address=fake.address(),
-                        password='password',
-                        email=username+'@stud.ntnu.no',
-                        about=fake.text(),
-                        birthday=fake.date_time_between_dates(datetime_start=None, datetime_end=None, tzinfo=None),
-                )
-                
-                user.groups.add(*groups)
-                user.save()
+        if delete:
+            News.objects.all().delete()
 
-            if delete:
-                News.objects.all().delete()
-
-            count = random.randint(10, 20)
-            print("Creating %d News" % count)
-            for i in range(count):
-                News.objects.create(
-                        headline=s(),
-                        body=g(),
-                        lead_paragraph=g()
-                        )
-
-            
-            count = random.randint(10, 20)
-            print("Creating %d Events" % count)
-            for i in range(count):
-                start = fake.date_time_between_dates(
-                    datetime_start=dt.now(),
-                    datetime_end=(dt.now()+td(30))
-                )
-
-                Event.objects.create(
+        count = random.randint(10, 20)
+        print("Creating %d News" % count)
+        for i in range(count):
+            News.objects.create(
                     headline=s(),
                     body=g(),
-                    lead_paragraph=g(),
-                    short_name=s(),
-                    event_start=start,
-                    event_end=start+td(hours=4),
-                    organizer=fake.name(),
-                    location=fake.address()
-                )
+                    lead_paragraph=g()
+                    )
+
+        
+        count = random.randint(10, 20)
+        print("Creating %d Events" % count)
+        for i in range(count):
+            start = fake.date_time_between_dates(
+                datetime_start=dt.now(),
+                datetime_end=(dt.now()+td(30))
+            )
+
+            Event.objects.create(
+                headline=s(),
+                body=g(),
+                lead_paragraph=g(),
+                short_name=s(),
+                event_start=start,
+                event_end=start+td(hours=4),
+                organizer=fake.name(),
+                location=fake.address()
+            )
 
 
 
