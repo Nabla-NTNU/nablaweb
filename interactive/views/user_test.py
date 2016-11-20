36aeb9c4cdcb329454f7a575cca19d2fcbbd8652
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2016-11-20 16:50:03 +0100

    Litt forenkling av user_test view
    Jeg klarte ikke å dy meg.

--- interactive/views/user_test.py
+++ interactive/views/user_test.py
@@ -1,6 +1,8 @@
+from collections import Counter
+from itertools import chain
 from django.views.generic import DetailView
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.http import Http404
 from django.template.response import TemplateResponse
 
@@ -23,28 +25,24 @@ class TestView(DetailView):
 
 
 def test_result(request, pk):
+    if request.method != "POST":
+        return redirect("user_test", pk=pk)
+
     test = get_object_or_404(Test, id=pk)
     questions = test.questions.all()
     format_string = "{}_alternative"
     answers = (request.POST.get(format_string.format(q.id)) for q in questions)
-    q_and_a = [
-        (q, int(answer))
+    answered_alternatives = (
+        q.alternatives.get(id=answer)
         for q, answer in zip(questions, answers)
         if answer is not None
-        ]
-    weighted_res = [q.alternatives.all()[a].get_target_with_weights() for q, a in q_and_a]
-    scores = []
-    results = test.results.all()
-    for res in results:
-        s = 0
-        for ws in weighted_res:
-            for w in ws:
-                if res in w:
-                    s += w[1]
-        scores.append(s)
-    max_score = max(scores)
-    index = scores.index(max_score)
-    result = results[index]
+    )
+    weighted_res = chain.from_iterable(a.get_target_with_weights() for a in answered_alternatives)
+    scores = Counter()
+    for target, weight in weighted_res:
+        scores[target] += weight
+
+    (result, _), = scores.most_common(1)
     context = {
         'base_template': test.template,
         'title': result.title,
