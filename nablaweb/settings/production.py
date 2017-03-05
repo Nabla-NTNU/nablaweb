b5654ba643d9cca9139dba030e66bfa4b71589c7
Herman Øie Kolden <kristoea@stud.ntnu.no>
2017-03-05 18:16:35 +0100

    Change db prefix from nabla_no to nablaweb

--- nablaweb/settings/production.py
+++ nablaweb/settings/production.py
@@ -22,7 +22,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
-        'KEY_PREFIX': 'nabla_no',
+        'KEY_PREFIX': 'nablaweb',
     }
 }
 
