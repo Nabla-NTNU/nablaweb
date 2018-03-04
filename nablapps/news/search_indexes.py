244206b46419f27a480d2212e05c4587858da90a
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-03-04 14:58:49 +0100

    Fixes news search index

--- nablapps/news/search_indexes.py
+++ nablapps/news/search_indexes.py
@@ -3,11 +3,8 @@ from .models import NewsArticle
 
 
 class NewsIndex(indexes.SearchIndex, indexes.Indexable):
-    headline = indexes.CharField(model_attr='headline')
-    lead_paragraph = indexes.CharField(model_attr='lead_paragraph')
-    body = indexes.CharField(model_attr='body')
-    created_date = indexes.DateTimeField(model_attr='created_date')
     text = indexes.CharField(document=True, use_template=True)
+    created_date = indexes.DateTimeField(model_attr='created_date')
 
     def get_model(self):
         return NewsArticle
