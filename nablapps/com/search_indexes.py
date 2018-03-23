b127a09158ed15a2213e0211bd36b12711f961f5
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-03-23 16:39:48 +0100

    Search for com

--- nablapps/com/search_indexes.py
+++ nablapps/com/search_indexes.py
@@ -3,10 +3,9 @@ from .models import ComPage
 
 
 class ComIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='com__name')
     description = indexes.CharField(model_attr='description')
-    last_changed_date = indexes.DateTimeField(model_attr='last_changed_date')
-    text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return ComPage
