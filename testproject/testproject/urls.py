20cb17e8ccffacc0754779b58843fe303fe068b0
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2018-09-22 22:15:03 +0200

    Replace usage of deprecated login and logout views with their class based counterparts

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -4,7 +4,7 @@ Urls for testproject for content
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.static import serve
 from django.views.generic import TemplateView
 from contentapps.image.views import view_markdown
@@ -15,8 +15,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^album/', include('contentapps.album.urls')),
     url(r'^blogg/', include('contentapps.blog.urls')),
-    url(r'^login/$', login, {'template_name': 'admin/login.html'}, name="auth_login"),
-    url(r'^logout/$', logout),
+    url(r'^login/$', LoginView.as_view(template_name='admin/login.html'), name="auth_login"),
+    url(r'^logout/$', LogoutView.as_view()),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^markdowntest/$', view_markdown),
 ]
