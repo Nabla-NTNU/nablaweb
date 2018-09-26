184cc0b610fa07dd6ab07af8be317267dc057a97
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2018-09-26 12:35:38 +0200

    Add docstrings for meeting_records app

--- nablapps/meeting_records/apps.py
+++ nablapps/meeting_records/apps.py
@@ -1,6 +1,10 @@
+"""
+App configs for meeting records
+"""
 from django.apps import AppConfig
 
 
 class MeetingRecordsConfig(AppConfig):
+    """Default app config for meeting_records"""
     name = 'nablapps.meeting_records'
     verbose_name = 'Referater'
