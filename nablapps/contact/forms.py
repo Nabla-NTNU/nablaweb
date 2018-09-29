a9c0341e157c3a6d7c077c0b1bf43813d1c206a2
Knut Andre Grytting Prestsveen <kaprests@stud.ntnu.no>
2018-09-30 00:00:39 +0200

    Adds spambot check

--- nablapps/contact/forms.py
+++ nablapps/contact/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    your_name = forms.CharField(label='Ditt navn', max_length=100)
-    email = forms.EmailField(label='Din e-post:', max_length=100)
+    your_name = forms.CharField(label='Ditt navn', max_length=100, required=True)
+    email = forms.EmailField(label='Din e-post:', max_length=100, required=True)
     subject = forms.CharField(label='Emne', max_length=100)
-    message = forms.CharField(label='Melding', widget=forms.Textarea)
+    message = forms.CharField(label='Melding', widget=forms.Textarea, required=True)
+    spam_check = forms.FloatField(label='svar', max_value=20, required=True)
+
+    test_val = 0
+
+    your_name_val = ''
+    email_val = ''
+    subject_val = ''
+    message_val = ''
 
     def process(self):
         cd = self.cleaned_data
@@ -12,4 +20,13 @@ class ContactForm(forms.Form):
         message = cd['message']+'\n-'+ cd['your_name']
         email = cd['email']
         return subject, message, email
+
+    def get_answer(self):
+        cd = self.cleaned_data
+        answer = cd['spam_check']
+        return answer
+
+    def get_test_val(self):
+        test_val = self.test_val
+        return test_val
 
--- nablapps/contact/views.py
+++ nablapps/contact/views.py
@@ -3,31 +3,56 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail, BadHeaderError
 from .forms import ContactForm
 
+import random
+
+import random
+
 def contact(request):
-    if request.method == 'POST':
+    spam_check = False
+    if request.method != 'POST':
+        test_val = ContactForm.test_val = random.randint(0,20)
+        context = make_context(request, spam_check, test_val)
+        return render(request, 'contact/contact.html', context)
+    
+    else:
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
-            #Sende mail
-            subject, message, email = contact_form.process()
-            try:
-                send_mail(subject, message, email, ['webkom@nabla.ntnu.no'], fail_silently=False)
-            except BadHeaderError:
-                return HttpResponse('Invalid header found')
-            return HttpResponseRedirect('/contact/success/')
-
-    else:
-        if request.user.is_authenticated:
-            #last in skjema uten navn og e-post
-            contact_form = ContactForm(initial={'your_name': request.user.get_full_name(), 'email': request.user.email})
-            context = {'contact_form': contact_form}
-            return render(request, 'contact/contact.html', context)
-        else:
-            contact_form = ContactForm
-            context = {'contact_form': contact_form,}
-            return render(request, 'contact/contact.html', context)
-
+            #spam check
+            if contact_form.get_test_val() == contact_form.get_answer():
+                #Sende mail
+                subject, message, email = contact_form.process()
+                try:
+                    send_mail(subject, message, email, ['webkom@nabla.ntnu.no'], fail_silently=False)
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found')
+                return HttpResponseRedirect('/contact/success/')
+            else:
+                spam_check = True
+                test_val = ContactForm.test_val = random.randint(0,20)
+                context = make_context(request, spam_check, test_val)
+                return render(request, 'contact/contact.html', context)
+ 
+        
 
 def success(request):
     return render(request, 'contact/success.html')
 # Create your views here.
+
+#######################################################################
+
+#Not a view, returns appropriate context for contact view
+def make_context(request, spam_check, test_val):
+    if request.user.is_authenticated:
+        #skjema uten navn og e-post
+        contact_form = ContactForm(initial={'your_name': request.user.get_full_name(), 'email': request.user.email})
+        context = {'contact_form': contact_form, 'spam_check': spam_check, 'test_val': test_val}
+        return context
+    else:
+        #tomt skjema
+        contact_form = ContactForm
+        context = {'contact_form': contact_form, 'spam_check': spam_check, 'test_val': test_val}
+        return context
+
+
+
 
