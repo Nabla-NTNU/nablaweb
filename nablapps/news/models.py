9b21f851c39b48c9ab3099ed42a791eba58a2ff8
Marte Kaastad Hoiskar <martekho@stud.ntnu.no>
2019-03-04 21:33:19 +0100

    Changed the ingress and body admin windows.

--- nablapps/news/models.py
+++ nablapps/news/models.py
@@ -18,6 +18,8 @@ from django.urls import reverse
 from image_cropping.fields import ImageRatioField
 
 from content.models import PublicationManagerMixin, TimeStamped, WithPicture
+from ckeditor.fields import RichTextField
+
 
 
 class TextContent(models.Model):
@@ -28,11 +30,11 @@ class TextContent(models.Model):
         verbose_name="tittel",
         max_length=100,
         blank=True)
-    lead_paragraph = models.TextField(
+    lead_paragraph = RichTextField(
         verbose_name="ingress",
         blank=True,
         help_text="Vises på forsiden og i artikkelen")
-    body = models.TextField(
+    body = RichTextField(
         verbose_name="brødtekst",
         blank=True,
         help_text=(
--- nablaweb/settings/base.py
+++ nablaweb/settings/base.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = [
     'hitcount',
     'markdown_deux',
     'sekizai', # http://django-sekizai.readthedocs.org/en/latest/#
+    'ckeditor', #Flytt hvis du vil
 
     # Djangoting
     'django.contrib.admin',
