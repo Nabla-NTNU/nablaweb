e0e47d1758a0164993875aaad8dd4f6e73420d1e
kristoea <kristoea@stud.ntnu.no>
2019-03-26 00:13:47 +0100

    More foolproof tear down
    Also checks if the thumbnail file is created

--- nablapps/nabladet/tests.py
+++ nablapps/nabladet/tests.py
@@ -4,22 +4,21 @@ from django.conf import settings
 from .pdfthumbnailer import thumbnail_pdf
 
 import os
-from shutil import copyfile
+from shutil import copyfile, rmtree
 
 
 class ThumbnailerTestCase(TestCase):
 
     def setUp(self):
         source = os.path.join(os.path.dirname(__file__), 'test_static/test_nabla.pdf')
-        self.dir = os.path.join(settings.MEDIA_ROOT, 'thumbnails/nabladet/test')
-        os.mkdir(self.dir)
-        self.test_pdf = os.path.join(self.dir, 'test_nabla.pdf')
+        self.test_dir = os.path.join(settings.MEDIA_ROOT, 'thumbnails/nabladet/test')
+        os.mkdir(self.test_dir)
+        self.test_pdf = os.path.join(self.test_dir, 'test_nabla.pdf')
         copyfile(source, self.test_pdf)
 
     def test_thumbnailer(self):
-        self.test_thumbnail = thumbnail_pdf(self.test_pdf)
+        test_thumbnail = thumbnail_pdf(self.test_pdf)
+        assert os.path.isfile(test_thumbnail), 'Did not create thumbnail'
 
     def tearDown(self):
-        os.remove(self.test_thumbnail)
-        os.remove(self.test_pdf)
-        os.rmdir(self.dir)
+        rmtree(self.test_dir, ignore_errors=True)
