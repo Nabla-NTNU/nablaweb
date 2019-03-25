3ccba7b1fe010b4e16eaa3cbf4645abde1ac032b
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-03-25 14:15:04 +0100

    Simply adds a comment with a TODO
    A somewhat unnecessary commit, but it's here just so I don't have to
    rethink this again.

--- nablapps/core/views.py
+++ nablapps/core/views.py
@@ -41,6 +41,7 @@ class FrontPageView(FlatPageMixin, TemplateView):
         return context
 
     def _add_news(self, context):
+		# TODO: use gte lookup to get only news with bumptime in the past
         news_list = FrontPageNews.objects.filter(visible=True)
         context['main_news'] = news_list.first()
         context['news_list_1'] = news_list[1:3]
