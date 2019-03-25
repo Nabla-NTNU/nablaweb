f46f906242107ca9c8f8e5163dfa80e87771ca9b
kristoea <kristoea@stud.ntnu.no>
2019-03-25 23:55:52 +0100

    Do file operations in appropriate media folder
    To avoid write permission problems

--- nablapps/nabladet/tests.py
+++ nablapps/nabladet/tests.py
@@ -1,17 +1,25 @@
 from django.test import TestCase
+from django.conf import settings
 
 from .pdfthumbnailer import thumbnail_pdf
 
 import os
+from shutil import copyfile
 
 
 class ThumbnailerTestCase(TestCase):
 
     def setUp(self):
-        self.test_pdf = os.path.join(os.path.dirname(__file__), 'test_static/test_nabla.pdf')
+        source = os.path.join(os.path.dirname(__file__), 'test_static/test_nabla.pdf')
+        self.dir = os.path.join(settings.MEDIA_ROOT, 'thumbnails/nabladet/test')
+        os.mkdir(self.dir)
+        self.test_pdf = os.path.join(self.dir, 'test_nabla.pdf')
+        copyfile(source, self.test_pdf)
 
     def test_thumbnailer(self):
         self.test_thumbnail = thumbnail_pdf(self.test_pdf)
 
     def tearDown(self):
         os.remove(self.test_thumbnail)
+        os.remove(self.test_pdf)
+        os.rmdir(self.dir)
