6bab6d98677da169d9b321cef25aa2b1fd92fa9f
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-08-21 01:50:15 +0200

    Gets rid of all migrations for the qrTickets app, and makes new.
    Hopefully this migration will be appliable on the server

--- nablapps/qrTickets/models.py
+++ nablapps/qrTickets/models.py
@@ -18,7 +18,7 @@ class QrTicket(models.Model):
     event = models.ForeignKey(QrEvent, on_delete=models.CASCADE, related_name='ticket_set')
     email = models.EmailField(blank=False)
     registered = models.BooleanField(default=False)
-    ticket_id = models.CharField(max_length=200, default="not valid ticket")
+    ticket_id = models.CharField(max_length=200)
 
 
     def __str__(self):
