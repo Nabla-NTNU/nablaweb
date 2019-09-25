c1ac5ac1dea3407948524ec57eef2fb0829aa5b9
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2019-09-25 15:10:18 +0200

    Changes nabladet migration for move to protected media
    A custom migration would cause an error if the protected media directory existed from before

--- nablapps/nabladet/migrations/0011_protect_nablad.py
+++ nablapps/nabladet/migrations/0011_protect_nablad.py
@@ -10,10 +10,15 @@ from nablapps.nabladet.models import Nablad
 
 
 def move_to_protected(apps, schema_editor):
+    """Move existing nablads from MEDIA_ROOT/nabladet to PROTECTED_MEDIA_ROOT/nabladet
+    This is really a one-off, because we move existing nablads to protect """
+
     src = os.path.join(settings.MEDIA_ROOT, 'nabladet')
-    if not os.path.exists(src):
-        os.makedirs(src)
     dst = os.path.join(settings.PROTECTED_MEDIA_ROOT, 'nabladet')
+
+    if not os.path.exists(src) or os.path.exists(dst):
+        # Nothing to to
+        return
     move(src, dst)
 
 
