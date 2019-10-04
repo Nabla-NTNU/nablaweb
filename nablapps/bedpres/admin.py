888375b3c422e20a0d05d990cf50029a75a7f600
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2019-10-04 15:25:08 +0200

    Adds migration that removes the bedpres model

--- nablapps/bedpres/admin.py
+++ nablapps/bedpres/admin.py
@@ -1,35 +1,3 @@
 """
 Admin interface for bedpres app
 """
-from django.contrib import admin
-from image_cropping import ImageCroppingMixin
-from nablapps.news.admin import add_to_frontpage
-from .models import BedPres
-
-
-@admin.register(BedPres)
-class BedPresAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    """Admin for BedPres model"""
-    fields = (
-        "picture",
-        "cropping",
-        "bpcid",
-        "headline",
-        "slug",
-        "company",
-        "short_name",
-        "lead_paragraph",
-        "body",
-        "organizer",
-        "location",
-        "event_start",
-        "event_end",
-        "registration_required",
-        "registration_deadline",
-        "registration_start",
-        "deregistration_deadline",
-        "places",
-        "has_queue",
-    )
-    prepopulated_fields = {"slug": ("headline",)}
-    actions = [add_to_frontpage]
--- nablapps/bedpres/models.py
+++ nablapps/bedpres/models.py
@@ -1,44 +1,4 @@
 """
 BPC-bedpres as a django model
 """
-from django.urls import reverse
-from django.db import models
 
-from nablapps.events.models.abstract_event import AbstractEvent
-from nablapps.jobs.models import Company
-from .bpcmixin import BPCEventMixin
-
-
-class BedPres(BPCEventMixin, AbstractEvent):
-    """
-    Model representing a bedpress at BPC.
-
-    It implements the same "interface" as the Event model in the events app.
-    It also adds the connection to BPC and a connection to a company in the jobs app.
-    """
-
-    bpcid = models.CharField(
-        verbose_name="BPC-id",
-        max_length=16,
-        unique=True,
-        blank=False,
-        help_text=("Dette er id'en som blir brukt internt hos BPC. "
-                   "Ikke endre den hvis du ikke vet hva du gjør."))
-    company = models.ForeignKey(
-        Company,
-        verbose_name="Bedrift",
-        blank=False,
-        on_delete=models.CASCADE,
-        help_text="Hvilken bedrift som står bak bedriftspresentasjonen")
-
-    class Meta:
-        verbose_name = "bedriftspresentasjon"
-        verbose_name_plural = "bedriftspresentasjoner"
-
-    def get_registration_url(self):
-        """Get the url used for registering for the event"""
-        return reverse('bedpres_registration', kwargs={'pk': self.pk})
-
-    def get_absolute_url(self):
-        """Get canonical url of object (used by django)"""
-        return reverse("bedpres_detail", kwargs={'pk': self.pk, 'slug': self.slug})
