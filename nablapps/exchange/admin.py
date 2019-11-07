5958b072e6cf10165e0744d956b1449c27f81ad6
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-11-07 12:44:18 +0100

    Enables adding exchange news to the frontpage

--- nablapps/exchange/admin.py
+++ nablapps/exchange/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from image_cropping import ImageCroppingMixin
+from nablapps.core.admin import ChangedByMixin
 from .models import University, Exchange, Info, ExchangeNewsArticle
+from nablapps.news.admin import add_to_frontpage
 
 
 class ExchangeAdmin(admin.ModelAdmin):
@@ -28,9 +31,16 @@ class InfoAdmin(admin.ModelAdmin):
     class Meta:
         model = Info
 
+
+class ExchangeNewsAdmin(ImageCroppingMixin, ChangedByMixin, admin.ModelAdmin):
+    """Admin interface for NewsArticle"""
+    prepopulated_fields = {"slug": ("headline", )}
+    actions = [add_to_frontpage]
+
+
 admin.site.register(Exchange, ExchangeAdmin)
 admin.site.register(University, UniversityAdmin)
 admin.site.register(Info, InfoAdmin)
-admin.site.register(ExchangeNewsArticle)
+admin.site.register(ExchangeNewsArticle, ExchangeNewsAdmin)
 
 # Register your models here.
