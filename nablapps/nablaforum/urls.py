9c80ca15eb0fce94de202fda87268b06d5b9767a
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-01-02 02:02:40 +0100

    Adds header and updates style
    Also some clean up

--- nablapps/nablaforum/urls.py
+++ nablapps/nablaforum/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   # path('', IndexView.as_view(), name="forum-index"),
+    path('<channel_id>/<thread_id>/', MainView.as_view(), name="forum-main"),
     path('create_new_channel/', CreateChannelView.as_view(), name="create-channel"),
     path('browse_channels/', BrowseChannelsView.as_view(), name="browse-channels"),
-   # path('<channel_id>/', ChannelIndexView.as_view(), name="channel-index"),
-   # path('<channel_id>/<thread_id>/', ThreadView.as_view(), name="thread-view"),
-
-    path('<channel_id>/<thread_id>/', MainView.as_view(), name="forum-main"),
 ]
