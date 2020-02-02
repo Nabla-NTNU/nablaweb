4e12e78a87710d088a7c0c791db3c6aaf42afb27
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-02-02 17:50:14 +0100

    Makes the unique validation work in application form

--- nablapps/apply_committee/views.py
+++ nablapps/apply_committee/views.py
@@ -1,5 +1,5 @@
 from django.forms import (HiddenInput, ModelForm, Textarea, TextInput,
-                          formset_factory)
+                          formset_factory, ValidationError)
 from django.views.generic.edit import FormView
 
 from .models import Application, ApplicationRound, Committee
@@ -11,25 +11,42 @@ class ApplicationForm(ModelForm):
         exclude = ["application_round", "applicant"]
         widgets = {
             "application_text": TextInput(),
-            "priority": HiddenInput()
+            "priority": HiddenInput(),
         }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, application_round, applicant, **kwargs):
+        self.application_round = application_round
+        self.applicant = applicant
         super().__init__(*args, **kwargs)
         self.fields["committee"].required = False
 
+    def full_clean(self):
+        super().full_clean()
+        try:
+            self.instance.applicant = self.applicant
+            self.instance.application_round = self.application_round
+            self.instance.validate_unique()
+        except ValidationError as e:
+            self._update_errors(e)
 
 class ApplicationView(FormView):
     num_forms = 3  # Number of forms in formset
     form_class = formset_factory(ApplicationForm, extra=0)
     template_name = "apply_committee/apply.html"
-    initial = [{"priority": i+1} for i in range(num_forms)]
+    initial = [{"priority": i+1, "application_text": "jo"} for i in range(num_forms)]
 
     def setup(self, request, *args, **kwargs):
-        print("setup")
-        return super().setup(request, *args, **kwargs)
-        print("setup!!!!!!!!")
         self.application_round = ApplicationRound.get_current()
+        return super().setup(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        # form_kwargs are passed by formset to the underlying form
+        kwargs['form_kwargs'] = {
+            'applicant': self.request.user,
+            'application_round': self.application_round
+            }
+        return kwargs
 
     def form_valid(self, formset):
         for form in formset:
@@ -46,7 +63,6 @@ class ApplicationView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-#        application_round = ApplicationRound.get_current()
         context["application_round"] = self.application_round
         context["formset"] = context.pop("form", None)  # Give a more intuitive name
         return context
