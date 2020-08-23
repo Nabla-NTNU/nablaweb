b916c6cdca050cc8bf5b97042abc201a596f1a05
Amund Eggen Svandal <aesvandal@gmail.com>
2020-08-23 09:44:07 +0200

    Tag test with 'needs_imagemagick'
    To exclude this test when testing simply pass
    --exclude-tag=needs_imagemagick to the test runner i.e.
    `pipenv run python manage.py test --exclude-tag=needs_imagemagick`

--- nablapps/nabladet/tests.py
+++ nablapps/nabladet/tests.py
@@ -5,7 +5,7 @@ import os
 from shutil import copyfile, rmtree
 
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, tag
 
 from .pdfthumbnailer import thumbnail_pdf
 
@@ -24,6 +24,9 @@ class ThumbnailerTestCase(TestCase):
         self.test_pdf = os.path.join(self.test_dir, "test_nabla.pdf")
         copyfile(source, self.test_pdf)
 
+    # Indicate that this test needs imagemagick to run. This allows
+    # excluding it on platforms that do not have it installed.
+    @tag("needs_imagemagick")
     def test_thumbnailer(self):
         """
         Did the thumbnail creation work?
@@ -31,7 +34,11 @@ class ThumbnailerTestCase(TestCase):
         This will fail if ImageMagick is not correctly installed
         """
         test_thumbnail = thumbnail_pdf(self.test_pdf)
-        assert os.path.isfile(test_thumbnail), "Did not create thumbnail"
+        assert os.path.isfile(test_thumbnail), (
+            "Did not create thumbnail. This test requires ImageMagick to run. "
+            "If you do not have it installed, you can skip this test by passing "
+            "`--exclude-tag=needs_imagemagick` to `manage.py test`"
+        )
 
     def tearDown(self):
         rmtree(self.test_dir, ignore_errors=True)
