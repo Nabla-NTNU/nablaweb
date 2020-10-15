7553938b8f17c32fcfea4e9804797c0c94e15494
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-10-16 00:08:34 +0200

    Removes unused import, fixes formatting.

--- nablapps/qrTickets/views.py
+++ nablapps/qrTickets/views.py
@@ -4,7 +4,7 @@ import string
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.mail import BadHeaderError, send_mail
 from django.http import HttpResponse
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
 from django.views.generic import DetailView, ListView, TemplateView
