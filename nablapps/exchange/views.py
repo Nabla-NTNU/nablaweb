babc69f1ab77ac0f4b0eb3eb0f8e9afc11b5b1c7
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-10-21 19:45:10 +0200

    Moves university list to the exchange frontpage (replaces news).

--- nablapps/exchange/views.py
+++ nablapps/exchange/views.py
@@ -1,58 +1,13 @@
 from django.db.models import Q
 from django.urls import reverse
 from django.views.generic import DetailView, ListView
-
 from nablapps.core.view_mixins import AdminLinksMixin
-
 from .models import RETNINGER, Exchange, ExchangeNewsArticle, Info, University
-
-# temporary
 
 
 class ExchangeFrontpageView(ListView):
-    template_name = "exchange/exchange_frontpage.html"
-    model = ExchangeNewsArticle
-    context_object_name = "news_list"
-    paginate_by = 5
-    queryset = ExchangeNewsArticle.objects.order_by("-pk")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        ex_list_url = reverse("ex_list")
-        ex_news_url = reverse("ex_news")
-        # context['news_list'] = exchange_news
-        context["ex_list_url"] = ex_list_url
-        context["ex_news_url"] = ex_news_url
-        return context
-
-
-class ExchangeNewsView(ListView):
-    template_name = "exchange/exchange_news.html"
-    model = ExchangeNewsArticle
-    context_object_name = "news_list"
-    paginate_by = 8
-    queryset = ExchangeNewsArticle.objects.order_by("-pk")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        ex_frontpage_url = reverse("ex_frontpage")
-        ex_list_url = reverse("ex_list")
-        context["ex_frontpage_url"] = ex_frontpage_url
-        context["ex_list_url"] = ex_list_url
-        return context
-
-
-class ExchangeNewsDetailView(AdminLinksMixin, DetailView):
-    """Show a single news-article"""
-
-    model = ExchangeNewsArticle
-    context_object_name = "news"
-    template_name = "exchange/ex_news_detail.html"
-
-
-class ExchangeListView(ListView):
     model = University
-    template_name = "exchange/ex_list.html"
+    template_name = "exchange/exchange_frontpage.html"
     context_object_name = "ex_list"
 
     def get_queryset(self):
@@ -66,10 +21,32 @@ class ExchangeListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ex_frontpage_url = reverse("ex_frontpage")
-        context["ex_frontpage_url"] = ex_frontpage_url
+        ex_news_url = reverse("ex_news")
+        context["ex_news_url"] = ex_news_url
         context["retninger"] = [long_name.capitalize() for _, long_name in RETNINGER]
         return context
+
+
+class ExchangeNewsView(ListView):
+    template_name = "exchange/exchange_news.html"
+    model = ExchangeNewsArticle
+    context_object_name = "news_list"
+    paginate_by = 8
+    queryset = ExchangeNewsArticle.objects.order_by("-pk")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ex_frontpage_url = reverse("ex_frontpage")
+        context["ex_frontpage_url"] = ex_frontpage_url
+        return context
+
+
+class ExchangeNewsDetailView(AdminLinksMixin, DetailView):
+    """Show a single news-article"""
+
+    model = ExchangeNewsArticle
+    context_object_name = "news"
+    template_name = "exchange/ex_news_detail.html"
 
 
 class UnivDetailView(DetailView):
