350ab6e98c7c3db23b3a07f0596a3cec57b4a923
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-10-21 19:45:10 +0200

    Moves university list to the exchange frontpage (replaces news).

--- nablapps/exchange/urls.py
+++ nablapps/exchange/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import (
     ExchangeFrontpageView,
-    ExchangeListView,
     ExchangeNewsDetailView,
     ExchangeNewsView,
     InfoDetailView,
@@ -13,7 +12,6 @@ urlpatterns = [
     path("", ExchangeFrontpageView.as_view(), name="ex_frontpage"),
     path("info/(<int:pk>/", InfoDetailView.as_view(), name="info_detail"),
     path("<int:pk>/", UnivDetailView.as_view(), name="ex_detail_list"),
-    path("exchange-list/", ExchangeListView.as_view(), name="ex_list"),
     path("exchange-news/", ExchangeNewsView.as_view(), name="ex_news"),
     path(
         "exchange-news/detail/<int:pk>/<str:slug>/",
