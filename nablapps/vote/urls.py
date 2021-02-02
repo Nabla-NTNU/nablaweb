069e6c6e382a3da4f3951768bea89a6e1878146f
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2021-02-02 14:30:31 +0100

    Fixes formatting

--- nablapps/vote/urls.py
+++ nablapps/vote/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (
+from .views import (  # submit_vote,
     ActiveVotingList,
     CreateVoting,
     Vote,
@@ -9,7 +9,6 @@ from .views import (
     VotingList,
     activate_voting,
     deactivate_voting,
-    #submit_vote,
 )
 
 urlpatterns = [
@@ -17,7 +16,9 @@ urlpatterns = [
     path("admin/<int:pk>/list/", VotingList.as_view(), name="voting-list"),
     path("admin/<int:pk>/create_voting/", CreateVoting.as_view(), name="create-voting"),
     path(
-        "admin/detail/<int:pk>/<int:event_id>/", VotingDetail.as_view(), name="voting-detail"
+        "admin/detail/<int:pk>/<int:event_id>/",
+        VotingDetail.as_view(),
+        name="voting-detail",
     ),
     path("activate/<int:pk>/<int:event_id>/", activate_voting, name="activate-voting"),
     path(
@@ -31,5 +32,5 @@ urlpatterns = [
         name="active-voting-list",
     ),
     path("<int:pk>/", Vote.as_view(), name="voting-vote"),
-    #path("<int:voting_id>/", submit_vote, name="submit-vote"),
+    # path("<int:voting_id>/", submit_vote, name="submit-vote"),
 ]
