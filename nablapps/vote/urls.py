470f6e501e73e480cc4fae9610a8404a63f4ef45
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2021-02-02 14:28:05 +0100

    Cleans up views and urls a bit

--- nablapps/vote/urls.py
+++ nablapps/vote/urls.py
@@ -9,27 +9,27 @@ from .views import (
     VotingList,
     activate_voting,
     deactivate_voting,
-    submit_vote,
+    #submit_vote,
 )
 
 urlpatterns = [
-    path("event_list/", VotingEventList.as_view(), name="voting-event-list"),
-    path("event/<int:pk>/list/", VotingList.as_view(), name="voting-list"),
+    path("admin/", VotingEventList.as_view(), name="voting-event-list"),
+    path("admin/<int:pk>/list/", VotingList.as_view(), name="voting-list"),
+    path("admin/<int:pk>/create_voting/", CreateVoting.as_view(), name="create-voting"),
     path(
-        "event/<int:pk>/active-list/",
-        ActiveVotingList.as_view(),
-        name="active-voting-list",
+        "admin/detail/<int:pk>/<int:event_id>/", VotingDetail.as_view(), name="voting-detail"
     ),
-    path("event/<int:pk>/create_voting/", CreateVoting.as_view(), name="create-voting"),
-    path(
-        "detail/<int:pk>/<int:event_id>/", VotingDetail.as_view(), name="voting-detail"
-    ),
-    path("vote/<int:pk>/<int:event_id>/", Vote.as_view(), name="voting-vote"),
-    path("vote/<int:pk>/<int:event_id>/submit/", submit_vote, name="submit-vote"),
     path("activate/<int:pk>/<int:event_id>/", activate_voting, name="activate-voting"),
     path(
         "deactivate/<int:pk>/<int:event_id>/",
         deactivate_voting,
         name="deactivate-voting",
     ),
+    path(
+        "",
+        ActiveVotingList.as_view(),
+        name="active-voting-list",
+    ),
+    path("<int:pk>/", Vote.as_view(), name="voting-vote"),
+    #path("<int:voting_id>/", submit_vote, name="submit-vote"),
 ]
