fa36d050b5a8387204ad3730139bedd309009cd6
Yawar Mahmood <yawarm@stud.ntnu.no>
2021-02-25 19:38:19 +0100

    black formatting

--- nablapps/accounts/signals.py
+++ nablapps/accounts/signals.py
@@ -10,18 +10,23 @@ from django.utils.safestring import mark_safe
 def login_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
         msg = f"Velkommen inn <strong>{escape(user.username)}</strong>"
-        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! " #reklame eSKE/SKE - fjernes etter
         messages.info(request, mark_safe(msg))
+
+    with suppress(MessageFailure):
+        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
+        messages.error(request, mark_safe(msg))
 
 
 def logout_message(sender, request, user, **kwargs):
     with suppress(MessageFailure):
         msg = f"<strong>{escape(user.username)} </strong> ble logget ut"
-        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! " #reklame eSKE/SKE - fjernes etter
         messages.info(request, mark_safe(msg))
+
+    with suppress(MessageFailure):
+        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
+        messages.error(request, mark_safe(msg))
 
 
 def register_signals():
     user_logged_in.connect(login_message)
     user_logged_out.connect(logout_message)
-
