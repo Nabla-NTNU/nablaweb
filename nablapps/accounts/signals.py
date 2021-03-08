1c89f3cf67936d3815d9e0529ff313bcafc4d8e1
Yawar Mahmood <yawarm@stud.ntnu.no>
2021-03-08 22:17:38 +0100

    fjerner eSKE reklame hver gang man logger på

--- nablapps/accounts/signals.py
+++ nablapps/accounts/signals.py
@@ -12,9 +12,9 @@ def login_message(sender, request, user, **kwargs):
         msg = f"Velkommen inn <strong>{escape(user.username)}</strong>"
         messages.info(request, mark_safe(msg))
 
-    with suppress(MessageFailure):
-        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
-        messages.error(request, mark_safe(msg))
+    # with suppress(MessageFailure):
+    #     msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
+    #     messages.error(request, mark_safe(msg))
 
 
 def logout_message(sender, request, user, **kwargs):
@@ -22,9 +22,9 @@ def logout_message(sender, request, user, **kwargs):
         msg = f"<strong>{escape(user.username)} </strong> ble logget ut"
         messages.info(request, mark_safe(msg))
 
-    with suppress(MessageFailure):
-        msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
-        messages.error(request, mark_safe(msg))
+    # with suppress(MessageFailure):
+    #     msg = f"Vil du få sjansen til å vinne gavekort på 250 kr, og mange andre kule premier? Sjekk ut innkallingen til eSKE da vel! "  # reklame eSKE/SKE - kan også brukes til annet
+    #     messages.error(request, mark_safe(msg))
 
 
 def register_signals():
