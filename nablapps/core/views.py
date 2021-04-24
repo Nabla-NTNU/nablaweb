f213c32d6aa69590d0e2ab42bf10be553c38e2a1
Yawar Mahmood <yawarm@stud.ntnu.no>
2021-04-25 00:56:30 +0200

    fixing linting

--- nablapps/core/views.py
+++ nablapps/core/views.py
@@ -61,8 +61,8 @@ class FrontPageView(FlatPageMixin, TemplateView):
         context["newuser_message"] = (
             False if self.request.user.is_authenticated else True
         )
-        #Uncomment when fadderperiode to display new student popup.
-        #context["newuser_popup"] = False if self.request.user.is_authenticated else True
+        # Uncomment when fadderperiode to display new student popup.
+        # context["newuser_popup"] = False if self.request.user.is_authenticated else True
         return context
 
     def _add_news(self, context):
