5ec6d8cea7dc40fa963c27f1d97bb5e604daf889
Yawar Mahmood <yawarm@stud.ntnu.no>
2021-08-19 19:26:22 +0200

    uncomment return context

--- nablapps/core/views.py
+++ nablapps/core/views.py
@@ -61,9 +61,9 @@ class FrontPageView(FlatPageMixin, TemplateView):
         context["newuser_message"] = (
             False if self.request.user.is_authenticated else True
         )
-        # # Uncomment when fadderperiode to display new student popup.
+        # Uncomment when fadderperiode to display new student popup.
         # context["newuser_popup"] = False if self.request.user.is_authenticated else True
-        # return context
+        return context
 
     def _add_news(self, context):
         news_list = FrontPageNews.objects.filter(visible=True).filter(
