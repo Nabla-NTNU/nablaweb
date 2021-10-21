8765171286b518da88d7f2cf9f40f045892c317f
Amund Eggen Svandal <aesvandal@gmail.com>
2021-10-21 22:00:48 +0200

    Add export active members action

--- nablapps/com/admin.py
+++ nablapps/com/admin.py
@@ -1,4 +1,9 @@
+from collections import defaultdict
+from functools import partial
+from io import StringIO
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import ComMembership, ComPage
 
@@ -11,9 +16,53 @@ class ComPageAdmin(admin.ModelAdmin):
 
 
 class ComMembershipAdmin(admin.ModelAdmin):
-    list_display = ("full_user_name", "user", "com", "joined_date", "short_description")
-    ordering = ["-com"]
-    list_filter = ["com"]
+    class Meta:
+        verbose_name = "Komitemedlem"
+        verbose_name_plural = "Komitemedlemmer"
+
+    def export_all_active_members(modeladmin, request, queryset):
+        """Download a file mapping each committee to a list of active members"""
+        # Map committee_name -> list of active members
+        committee_member_mapping = defaultdict(list)
+
+        all_members = ComMembership.objects.all()
+
+        # We ignore the queryset, and export all active members
+        active_members = all_members.select_related("user", "com").filter(
+            is_active=True, user__email__isnull=False
+        )
+
+        for membership in active_members:
+            committee_member_mapping[membership.com.name].append(membership.user.email)
+
+        output_stream = StringIO()
+        write_to_attachment = partial(print, file=output_stream)
+
+        # Write some stats
+        write_to_attachment(
+            f"Total members (active and inactive): {all_members.count()}"
+        )
+        write_to_attachment(f"Active members with emails: {active_members.count()}")
+        write_to_attachment(
+            f"#Committees with active members: {len(committee_member_mapping)}"
+        )
+        write_to_attachment(
+            f"Committees with active members: {', '.join(committee_member_mapping.keys())}"
+        )
+        write_to_attachment("\n\n")
+
+        for committee, emails in committee_member_mapping.items():
+            write_to_attachment(f"**{committee}**:")
+            for email in emails:
+                write_to_attachment(email)
+            write_to_attachment("------------------------\n")
+
+        output_stream.seek(0)
+        response = HttpResponse(output_stream, content_type="text")
+        response[
+            "Content-Disposition"
+        ] = "attachment; filename=active_committee_members.txt"
+        return response
 
     def short_description(self, com):
         return (com.story[:23] + "...") if len(com.story) > 25 else com.story
@@ -21,9 +70,13 @@ class ComMembershipAdmin(admin.ModelAdmin):
     def full_user_name(self, com):
         return com.user.get_full_name()
 
-    class Meta:
-        verbose_name = "Komitemedlem"
-        verbose_name_plural = "Komitemedlemmer"
+    export_all_active_members.short_name = "Eksportér alle aktive medlemmer"
+
+    list_display = ("full_user_name", "user", "com", "joined_date", "short_description")
+    list_select_related = ("user", "com")
+    ordering = ["-com"]
+    list_filter = ["com"]
+    actions = [export_all_active_members]
 
 
 class CommitteeAdmin(admin.ModelAdmin):
