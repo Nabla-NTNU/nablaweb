7405c8ad5228b90317fe068990ae8458238e32d6
Yawar Mahmood <yawarm@stud.ntnu.no>
2022-09-08 19:41:34 +0200

    fix: typos and unused import

--- nablapps/contact/views.py
+++ nablapps/contact/views.py
@@ -8,7 +8,7 @@ from django.urls import resolve
 
 from click import style
 
-from .forms import ContactForm, FeedbackForm, ForCompanyForm, RoomForm
+from .forms import ContactForm, FeedbackForm, RoomForm
 
 
 def contact(request):
