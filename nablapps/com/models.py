6c16256cb3015052f46dbe06d7c761d1b272a652
Amund Eggen Svandal <aesvandal@gmail.com>
2022-10-06 20:04:48 +0200

    Fix deprecated ugettext_lazy (4.0)

--- nablapps/com/models.py
+++ nablapps/com/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import Group
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from ckeditor.fields import RichTextField
 
