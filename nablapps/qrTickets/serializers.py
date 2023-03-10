4f463fdc33bc987885c309101eee43be24b57356
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2023-03-10 21:29:38 +0100

    Display scan history in QRTicket scan page

--- nablapps/qrTickets/serializers.py
+++ nablapps/qrTickets/serializers.py
@@ -6,5 +6,6 @@ from nablapps.qrTickets.models import QrTicket
 class QrTicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = QrTicket
-        fields = ["registered"]
+        fields = ["registered", "email", "ticket_id"]
+        read_only_fields = ["email", "ticket_id"]
         lookup_field = "ticket_id"
