505ca024c12853452df922f479733c886b37ce43
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2025-01-09 14:17:18 +0100

    (Hopefully) removed public application list

--- nablapps/apply_committee/urls.py
+++ nablapps/apply_committee/urls.py
@@ -5,11 +5,10 @@ from . import views
 # This file is included from main url.py under url '^application/'
 urlpatterns = [
     path("", views.ApplicationView.as_view(), name="apply-committee"),
-    path("list/", views.ApplicationListView.as_view(), name="list-applicants"),
     path(
-        "list/admin/",
+        "list/",
         views.AdminApplicationListView.as_view(),
-        name="admin-list-applicants",
+        name="list-applicants",
     ),
     path("confirmation/", views.ConfirmView.as_view(), name="confirm"),
     path("list/admin/export.csv/", views.generate_csv, name="csv"),
