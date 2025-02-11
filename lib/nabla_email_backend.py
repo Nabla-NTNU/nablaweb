2f61688cc52cf37f0cc2f9116551c9a1c339adf8
Alexander Mitrokhin <=>
2025-02-11 22:44:31 +0100

    Cleaned email backend

--- lib/nabla_email_backend.py
+++ lib/nabla_email_backend.py
@@ -1,32 +1,90 @@
+# Raw email encoding
+import base64
+from email.message import EmailMessage
+
 from django.core.mail.backends.base import BaseEmailBackend
 
-from lib import GCloudUtils as gcu
+# Google API authentication
+from google.oauth2 import service_account  # Authentication
+from googleapiclient.discovery import build  # API wrapper
+from googleapiclient.errors import HttpError  # Interpreting returned errors
+
+# Private key filepath
+SERVICE_ACCOUNT_FILE_PATH: str = "lib/privateKey.json"
+
+# Email of admin to be impersonated. Used as true from email.
+SUPER_ADMIN: str = "noreply@nabla.no"
+
+# Domain name of the soc website
+ROOT_DOMAIN: str = "nabla.no"
 
 
 class Nabla_email_backend(BaseEmailBackend):
-    handler = gcu.GCloudHandler()
+    SCOPES: list[str] = [
+        "https://www.googleapis.com/auth/gmail.send",
+    ]  # https://developers.google.com/admin-sdk/directory/v1/guides/authorizing < Relevant scopes
+
+    _service = None
+
+    # Generate credentials and object to send API calls
+    def __init__(self, **kwargs):
+        _creds = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE_PATH, scopes=self.SCOPES
+        ).with_subject(SUPER_ADMIN)
+        self._service = build("gmail", "v1", credentials=_creds)
+
+    def send_message(self, email_message):
+        mail = EmailMessage()
+        mail.set_content(email_message.body)
+
+        # Gmail overrides "From" field to be superuser. Therefore
+        #   CC them, and set the Reply-To to the actual FROM email
+        # mail["To"] = email_message.to
+        mail["To"] = (
+            "webkom@nabla.no"  # Override to send mail to outselves for debugging
+        )
+        mail["From"] = SUPER_ADMIN
+        mail["Subject"] = email_message.subject
+        mail["CC"] = email_message.cc + [email_message.from_email]
+        mail["BCC"] = email_message.bcc
+        mail["Reply-To"] = email_message.reply_to + [email_message.from_email]
+
+        # Encode email to binary
+        encoded_email = base64.urlsafe_b64encode(mail.as_bytes()).decode()
+        encoded_package = {"raw": encoded_email}
+
+        try:
+            # Send email using the object
+            email_attempt = (
+                self._service.users()
+                .messages()
+                .send(userId="me", body=encoded_package)
+                .execute()
+            )
+            print("Sent email of ID: %s" % email_attempt["id"])
+        except HttpError as error:
+            print("Error: %s" % error)
 
     def send_messages(self, email_messages):
         for email_message in email_messages:
-            self.handler.send_email(
-                # to = email_message.to,
-                to="webkom@nabla.no",  # FOR TESTING PURPOSES ONLY, RESET BEFORE MERGE
-                subject=email_message.subject,
-                message=email_message.body,
-                bcc=email_message.bcc,  # Gmail overrides author as
-                cc=email_message.cc
-                + [email_message.from_email],  # impersonated superuser.
-                reply_to=email_message.reply_to,  # Therefore we CC them instead.
-            )
+            self.send_message(email_message)
 
 
-# TODO: Fix DEFAULT_FROM_EMAIL                          Impossible. Added as CC and text in mail instead.
-# TODO: Fix sending to list of recipient addresses      We seem not to use send_message
+# Authenitcation is done by connecting the service account set up on the cloud.google.com
+#     console and set up with domain-wide authority in on the admin panel. This is what
+#     open() does. As we are doing sensitive admin work, we need to impersonate a superadmin,
+#     which is what .with_subject does.
 
-# TODO: Confirm connection not needed                   Can be implemented, not yer
-# TODO: Confirm headers not needed                      As of yet unused
+# This then needs to be built into a service object that actually makes the call. This is
+#     done (for the gmail API, version 1)
+#         self._service = build("gmail", "v1", credentials=_creds)
 
-# TODO: Add versioning to google lib & clean            Semantic major version locked down
-
-# TODO: See how lots of mail is sent by nablaweb atm    We seem not to send lots of mail atm
-# TODO: See django's send_mass_email()                  UNUSED
+# API calls are done by specifiying which API wr're using, and calling the HTTP request like
+#           email_attempt = (
+#               self._service.users()
+#               .messages()
+#               .send(userId="me", body=encoded_package)
+#               .execute()
+#           )
+#     Here we use the service object to access the groups API, run a get function giving it
+#     what it asks for in the documentation, and actually calling it by .excecute().
