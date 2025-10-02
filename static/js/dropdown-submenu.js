05f121c7edd5e2950fd8230f7c3cb815b7bdf227
Mikkel Vaaje-Kolstad <mikkelvk@hotmsil.com>
2025-10-02 20:00:54 +0200

    cleaned up dropdown subumenu script

--- static/js/dropdown-submenu.js
+++ static/js/dropdown-submenu.js
@@ -1,26 +1,25 @@
-
+// Script to handle dropdown submenus
 // $ runs the function through jQuery for it to be compatible with Bootstrap
+
 $(document).ready(function() {
 
     $(".dropdown-submenu > .dropdown-toggle").on("click", function(e) {
         e.preventDefault();   
         e.stopPropagation();  
 
-        const $submenu = $(this).next(".dropdown-menu");
-        const isOpen = $submenu.hasClass("show");
+        const submenu = $(this).next(".dropdown-menu");
+        const isOpen = submenu.hasClass("show");
 
-        // Toggle submenu
-        $submenu.toggleClass("show");
+        submenu.toggleClass("show");
 
-        // Update aria-expanded for accessibility
         $(this).attr("aria-expanded", !isOpen);
     });
 
     // Close all submenus when the main dropdown closes
     $(".dropdown").on("hide.bs.dropdown", function() {
+
         $(this).find(".dropdown-submenu > .dropdown-menu.show").removeClass("show");
 
-        // Reset aria-expanded for all submenu toggles
         $(this).find(".dropdown-submenu > .dropdown-toggle").attr("aria-expanded", "false");
     });
 });
