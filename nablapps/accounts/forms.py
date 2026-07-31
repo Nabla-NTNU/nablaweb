3d57ce3a9da5bea6cda679035cf1dbbdfd79dc60
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2026-07-31 15:55:09 +0200

    Populating fysmat_class dynamically, to avoid errors on new db

--- nablapps/accounts/forms.py
+++ nablapps/accounts/forms.py
@@ -77,16 +77,20 @@ class RegistrationForm(forms.Form):
 
     last_name = forms.CharField(label="Etternavn", required=True)
 
-    fysmat_class = forms.ChoiceField(
-        required=True,
-        label="Kull",
-        choices=[("", "Velg ditt fysmat-kull")]
-        + [
-            (m.name, f"Kull {m.starting_year}")
-            for m in FysmatClass.objects.order_by("-starting_year")
-        ][:5]
-        + [("class_international", "International")],
-    )
+    fysmat_class = forms.ChoiceField(required=True, label="Kull", choices=())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["fysmat_class"] = forms.ChoiceField(
+            required=True,
+            label="kull",
+            choices=[("", "Velg ditt fysmat-kull")]
+            + [
+                (m.name, f"Kull {m.starting_year}")
+                for m in FysmatClass.objects.order_by("-starting_year")
+            ][:5]
+            + [("class_international", "International")],
+        )
 
 
 # Forms for admin
